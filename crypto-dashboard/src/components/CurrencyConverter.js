539b8fb70062d9b2673d6aaaee6291354608263c
Jonny Soth <janysoth@gmail.com>
2023-05-10 05:19:54 -0500

    Create options value.

--- crypto-dashboard/src/components/CurrencyConverter.js
+++ crypto-dashboard/src/components/CurrencyConverter.js
@@ -1,21 +1,53 @@
-import React from "react";
+import { useState } from "react";
 import ExchangeRate from "./ExchangeRate";
 
 function CurrencyConverter() {
+  const currencies = ["BTC", "ETH", "USD", "XRP", "LTC", "ADA"];
+  // Set the value to option selected
+  const [chosenPrimaryCurrency, SetChosenPrimaryCurrency] = useState(null);
+
   return (
     <div className="currency-converter">
-      <h2>CurrencyConverter</h2>
+      <h2>Currency Converter</h2>
 
-      <div>
+      <div className="input-box">
         <table>
-          <body>
+          <tbody>
             <tr>
               <td>Primary Currency:</td>
               <td>
                 <input type="number" name="currency-amount-1" value={""} />
               </td>
+              <td>
+                <select
+                  value={""}
+                  name="currency-option-1"
+                  className="currency-options"
+                >
+                  {currencies.map((currency, _index) => (
+                    <option key={_index}>{currency}</option>
+                  ))}
+                </select>
+              </td>
             </tr>
-          </body>
+            <tr>
+              <td>Secondary Currency:</td>
+              <td>
+                <input type="number" name="currency-amount-2" value={""} />
+              </td>
+              <td>
+                <select
+                  value={""}
+                  name="currency-option-2"
+                  className="currency-options"
+                >
+                  {currencies.map((currency, _index) => (
+                    <option key={_index}>{currency}</option>
+                  ))}
+                </select>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
 
