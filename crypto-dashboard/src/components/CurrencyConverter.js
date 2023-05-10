bdc63a6832f961ec1e140b7bfc81a19b052a04df
Jonny Soth <janysoth@gmail.com>
2023-05-10 05:00:17 -0500

    Create a markup for NewsFeed, CurrencyCoverter, and ExchangeRate.

--- crypto-dashboard/src/components/CurrencyConverter.js
+++ crypto-dashboard/src/components/CurrencyConverter.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import ExchangeRate from "./ExchangeRate";
 
 function CurrencyConverter() {
   return (
     <div className="currency-converter">
-        CurrencyConverter
+      <h2>CurrencyConverter</h2>
+
+      <div>
+        <table>
+          <body>
+            <tr>
+              <td>Primary Currency:</td>
+              <td>
+                <input type="number" name="currency-amount-1" value={""} />
+              </td>
+            </tr>
+          </body>
+        </table>
+      </div>
+
+      <ExchangeRate />
     </div>
-  )
+  );
 }
 
 export default CurrencyConverter;
