c955d71baaa6c9249ad9238d3e27d5905591c51e
Jonny Soth <janysoth@gmail.com>
2023-05-12 04:47:43 -0500

    Use useEffect to only set primaryCurrency and secondaryCurrrency once.

--- crypto-dashboard/src/components/CurrencyConverter.js
+++ crypto-dashboard/src/components/CurrencyConverter.js
@@ -1,11 +1,28 @@
+<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
+=======
+import { useState, useEffect } from "react";
+>>>>>>> cb48c55 (Use useEffect to only set primaryCurrency and secondaryCurrrency once.)
 import ExchangeRate from "./ExchangeRate";
 
 const CurrencyConverter = () => {
   const [primaryCurrency, setPrimaryCurrency] = useState('');
   const [secondaryCurrency, setSecondaryCurrency] = useState('');
   const currencies = ["BTC", "ETH", "USD", "XRP", "LTC", "ADA"];
+<<<<<<< HEAD
 
+=======
+  const [primaryCurrency, setPrimaryCurrency] = useState(null);
+  const [secondaryCurrency, setSecondaryCurrency] = useState(null);
+
+  useEffect(() => {
+    console.log(primaryCurrency);
+  }, [primaryCurrency]);
+
+  useEffect(() => {
+    console.log(secondaryCurrency);
+  }, [secondaryCurrency]);
+>>>>>>> cb48c55 (Use useEffect to only set primaryCurrency and secondaryCurrrency once.)
 
   return (
     <div className="currency-converter">
@@ -13,6 +30,7 @@ const CurrencyConverter = () => {
 
       <div className="input-box">
         <table>
+<<<<<<< HEAD
           <tr>
             <td>Primary Currency:</td>
             <td>
@@ -49,6 +67,48 @@ const CurrencyConverter = () => {
               </select>
             </td>
           </tr>
+=======
+          <tbody>
+            <tr>
+              <td>Primary Currency</td>
+              <td>
+                <input type="number" name="currency-amount-1" value={""} />
+              </td>
+              <td>
+                <select
+                  value={primaryCurrency}
+                  name="currency-option-1"
+                  className="currency-options"
+                  onChange={(e) => setPrimaryCurrency(e.target.value)}
+                >
+                  {currencies.map((currency, _index) => (
+                    <option key={_index}>{currency}</option>
+                  ))}
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td>Secondary Currency</td>
+              <td>
+                <input type="number" name="currency-amount-2" value={""} />
+              </td>
+              <td>
+                <select
+                  value={secondaryCurrency}
+                  name="currency-option-2"
+                  className="currency-options"
+                  onChange={(event) =>
+                    setSecondaryCurrency(event.target.value)
+                  }
+                >
+                  {currencies.map((currency, _index) => (
+                    <option key={_index}>{currency}</option>
+                  ))}
+                </select>
+              </td>
+            </tr>
+          </tbody>
+>>>>>>> cb48c55 (Use useEffect to only set primaryCurrency and secondaryCurrrency once.)
         </table>
       </div>
 
