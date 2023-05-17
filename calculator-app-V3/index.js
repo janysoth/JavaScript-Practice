b7c1beb4bdd0333a2838cb7586b1b8e8d66a8ee0
Jonny Soth <janysoth@gmail.com>
2023-05-17 03:04:22 -0500

    Create a Calculator Class to have all of the functions that it has.

---
+++ calculator-app-V3/index.js
@@ -0,0 +1,41 @@
+// Create a Calculator Class: 
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+    }
+    // To Add all of the Functions Calculator Class Has: 
+    clear(){
+
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(number){
+
+    }
+
+    chooseOperation(operation){
+
+    }
+
+    compute(){
+
+    }
+
+    updateDisplay(){
+        
+    }
+
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
