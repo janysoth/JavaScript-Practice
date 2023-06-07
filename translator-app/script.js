2ac31658bc827d9c3afc55870be0e71e67a9fdc8
Jonny Soth <janysoth@gmail.com>
2023-06-07 02:47:44 -0500

    Refractor codes for script.js

--- translator-app/script.js
+++ translator-app/script.js
@@ -1,68 +1,79 @@
-const fromText = document.querySelector(".from-text"),
-    toText = document.querySelector(".to-text"),
-    exchageIcon = document.querySelector(".exchange"),
-    selectTag = document.querySelectorAll("select"),
-    icons = document.querySelectorAll(".row i");
-translateBtn = document.querySelector("button"),
+// Variable Declarations
+const fromText = document.querySelector(".from-text");
+const toText = document.querySelector(".to-text");
+const exchangeIcon = document.querySelector(".exchange");
+const selectTags = document.querySelectorAll("select");
+const icons = document.querySelectorAll(".row i");
+const translateBtn = document.querySelector("button");
 
-    selectTag.forEach((tag, id) => {
-        for (let country_code in countries) {
-            let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
-            let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
-            tag.insertAdjacentHTML("beforeend", option);
-        }
-    });
-
-exchageIcon.addEventListener("click", () => {
-    let tempText = fromText.value,
-        tempLang = selectTag[0].value;
-    fromText.value = toText.value;
-    toText.value = tempText;
-    selectTag[0].value = selectTag[1].value;
-    selectTag[1].value = tempLang;
+// Populating Select Tags
+selectTags.forEach((tag, id) => {
+    for (let country_code in countries) {
+        let selected =
+            (id === 0 && country_code === "en-GB") ||
+                (id === 1 && country_code === "hi-IN")
+                ? "selected"
+                : "";
+        let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+    }
 });
 
+// Exchange Icon Click Event
+exchangeIcon.addEventListener("click", () => {
+    [fromText.value, toText.value] = [toText.value, fromText.value];
+    [selectTags[0].value, selectTags[1].value] = [
+        selectTags[1].value,
+        selectTags[0].value,
+    ];
+});
+
+// From Text Keyup Event
 fromText.addEventListener("keyup", () => {
     if (!fromText.value) {
         toText.value = "";
     }
 });
 
+// Translate Button Click Event
 translateBtn.addEventListener("click", () => {
-    let text = fromText.value.trim(),
-        translateFrom = selectTag[0].value,
-        translateTo = selectTag[1].value;
+    let text = fromText.value.trim();
+    let translateFrom = selectTags[0].value;
+    let translateTo = selectTags[1].value;
     if (!text) return;
+
     toText.setAttribute("placeholder", "Translating...");
+
     let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
-    fetch(apiUrl).then(res => res.json()).then(data => {
-        toText.value = data.responseData.translatedText;
-        data.matches.forEach(data => {
-            if (data.id === 0) {
-                toText.value = data.translation;
-            }
+    fetch(apiUrl)
+        .then((res) => res.json())
+        .then((data) => {
+            toText.value = data.responseData.translatedText;
+            data.matches.forEach((match) => {
+                if (match.id === 0) {
+                    toText.value = match.translation;
+                }
+            });
+            toText.setAttribute("placeholder", "Translation");
         });
-        toText.setAttribute("placeholder", "Translation");
-    });
 });
 
-icons.forEach(icon => {
+// Icons Click Event
+icons.forEach((icon) => {
     icon.addEventListener("click", ({ target }) => {
         if (!fromText.value || !toText.value) return;
+
         if (target.classList.contains("fa-copy")) {
-            if (target.id == "from") {
-                navigator.clipboard.writeText(fromText.value);
-            } else {
-                navigator.clipboard.writeText(toText.value);
-            }
+            let textToCopy = target.id === "from" ? fromText.value : toText.value;
+            navigator.clipboard.writeText(textToCopy);
         } else {
             let utterance;
-            if (target.id == "from") {
+            if (target.id === "from") {
                 utterance = new SpeechSynthesisUtterance(fromText.value);
-                utterance.lang = selectTag[0].value;
+                utterance.lang = selectTags[0].value;
             } else {
                 utterance = new SpeechSynthesisUtterance(toText.value);
-                utterance.lang = selectTag[1].value;
+                utterance.lang = selectTags[1].value;
             }
             speechSynthesis.speak(utterance);
         }
