e8166f0cae056df9db324b296950ca4320c6e9c6
Jonny Soth <janysoth@gmail.com>
2023-06-09 05:03:23 -0500

    Create another vue app to control another div.

--- vue.js/script.js
+++ vue.js/script.js
@@ -4,13 +4,27 @@ const app = Vue.createApp({
     // template: '<h2>I am a template</h2>'
 
     // Create function and must return an object:
+    // Then you can call the object properties in HTML using {{}}
     data() {
         return {
-            title: 'The Final Empire'
+            title: 'The Final Empire',
+            author: 'Jonny Vorn Soth',
+            age: 45
         }
     }
 
 })
+
+// Create another Vue app and mount to a different div#
+const car = Vue.createApp({
+    data() {
+        return {
+            make: 'Honda',
+            model: 'Civic',
+            year: '2012'
+        }
+    }
+}).mount('#car')
 
 // To mount the Vue app to #app
 // And control everything inside the div #app
