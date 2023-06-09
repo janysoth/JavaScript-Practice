90b2663c90765d11830d3fc4a8e5f622f4a9caed
Jonny Soth <janysoth@gmail.com>
2023-06-09 04:53:42 -0500

    Learn how to create data function in vue.js.

--- vue.js/script.js
+++ vue.js/script.js
@@ -1,7 +1,15 @@
 // Creating a Vue App
 const app = Vue.createApp({
-    // data, functions
-    template: '<h2>I am a template</h2>'
+    // data functions
+    // template: '<h2>I am a template</h2>'
+
+    // Create function and must return an object:
+    data() {
+        return {
+            title: 'The Final Empire'
+        }
+    }
+
 })
 
 // To mount the Vue app to #app
