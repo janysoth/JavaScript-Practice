e4cc041f702bde516c16f38e2fdcbc1a2a906301
Jonny Soth <janysoth@gmail.com>
2023-06-11 00:13:16 -0500

    install npm

--- Vue-Projects/script.js
+++ Vue-Projects/script.js
@@ -1,53 +1,52 @@
 // Creating a Vue App
 // Similar to creating an object
 const app = Vue.createApp({
-    // data functions
-    // template: '<h2>I am a template</h2>'
+  // data functions
+  // template: '<h2>I am a template</h2>'
 
-    // Create function and must return an object:
-    // Then you can call the object properties in HTML using {{}}
-    data() {
-        return {
-            showBooks: true,
-            title: 'The Final Empire',
-            author: 'Jonny Vorn Soth',
-            age: 45
-        }
+  // Create function and must return an object:
+  // Then you can call the object properties in HTML using {{}}
+  data() {
+    return {
+      showBooks: true,
+      title: "The Final Empire",
+      author: "Jonny Vorn Soth",
+      age: 45,
+    };
+  },
+  // Functions Properties
+  methods: {
+    changeTitle(title) {
+      // this.title = 'Words of Randiance'
+      this.title = title;
     },
-    // Functions Properties
-    methods: {
-        changeTitle(title) {
-            // this.title = 'Words of Randiance'
-            this.title = title
-        },
 
-        // Change showBooks Boolean \
-        // This method being called in index.html
-        toggleShowBooks() {
-            this.showBooks = !this.showBooks
-        }
-    }
-
-})
+    // Change showBooks Boolean \
+    // This method being called in index.html
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+  },
+});
 
 // Create another Vue app and mount to a different div#
 const car = Vue.createApp({
-    data() {
-        return {
-            showCars: true,
-            make: 'Honda',
-            model: 'Civic',
-            year: '2012'
-        }
-    },
+  data() {
+    return {
+      showCars: true,
+      make: "Honda",
+      model: "Civic",
+      year: "2012",
+    };
+  },
 
-    methods: {
-        toggleShowCars() {
-            this.showCars = !this.showCars
-        }
-    }
-}).mount('#car')
+  methods: {
+    toggleShowCars() {
+      this.showCars = !this.showCars;
+    },
+  },
+}).mount("#car");
 
 // To mount the Vue app to #app
 // And control everything inside the div #app
-app.mount('#app')
+app.mount("#app");
