8fc0cebf6d1d9ba0aebd068eea99d7d6a1721e1a
Jonny Soth <janysoth@gmail.com>
2023-06-18 01:45:29 -0500

    Create addNewTodo, toggleDone, and removeTodo functions.

---
+++ Vue-Projects/todo-app/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <h1>Vue 3 To-Do App</h1>
+    <!-- To Prevent the page to reload -->
+    <form @submit.prevent="addNewTodo">
+      <label>New To-Do</label>
+      <!-- Bind newTodo using model (One-Way Data Binding)-->
+      <!-- Anything changes in the input, it will update newTodo -->
+      <input v-model="newTodo" name="newTodo" />
+      <button>Submit</button>
+    </form>
+    <ul>
+      <!-- To Pass the value, need v-bind (:) -->
+      <li v-for="(todo, index) in todos" :key="todo.id" class="todo">
+        <!-- Use class binding to change the class when Mark as Done -->
+        <!-- a .done class is created when todo.done is true -->
+        <h3 :class="{ done: todo.done }" @click="toggleDone(todo)">
+          {{ todo.content }}
+        </h3>
+        <!-- To Remove todo using index (slice from the array) -->
+        <button @click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  // To use composition API, use setup property
+  // Need to return any functions or const
+  setup() {
+    // ref is an object wrapper
+    // To Change, use .value
+    const newTodo = ref("");
+    const todos = ref([]);
+
+    function addNewTodo() {
+      todos.value.push({
+        id: Date.now(),
+        done: false,
+        content: newTodo.value,
+      });
+
+      newTodo.value = "";
+    }
+
+    // To Mark As Done with strikethrough:
+    function toggleDone(todo) {
+      todo.done = !todo.done;
+    }
+
+    // To Remove todo from the array by using splice method:
+    // Splice: remove One specific index from the array:
+    function removeTodo(index) {
+      todos.value.splice(index, 1);
+    }
+    return {
+      newTodo,
+      todos,
+      addNewTodo,
+      toggleDone,
+      removeTodo,
+    };
+  },
+};
+</script>
+
+<!-- <style>
+</style> -->
