ad51e9b0b11941ec04625cbe062c74386ddec794
Jonny Soth <janysoth@gmail.com>
2023-07-08 05:21:59 -0500

    Create CalculatorApp.vue and install bootstrap to style.

--- Vue.js/vue-todo/src/views/CalculatorApp.vue
+++ Vue.js/vue-todo/src/views/CalculatorApp.vue
@@ -1,24 +1,15 @@
 <template>
-  <div class="calculator-app">
-    <h1>This is a Calculator App</h1>
+  <div
+    class="p-3"
+    style="max-width: 400px; margin: 50px auto; background: #234"
+  >
+    <!-- Calculator Result -->
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .calculator-app {
-    display: flex;
-    flex-direction: column;
-    max-width: 500px;
-    width: 100%;
-    margin: 0 auto;
-    padding: 40px 16px;
-  }
+<script setup>
+</script>
 
-  h1 {
-    margin-bottom: 16px;
-    text-align: center;
-    text-decoration: underline;
-  }
-}
+<style lang="scss" scoped>
 </style>
+
