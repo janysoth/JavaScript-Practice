fb4f44704394d9926f87fbd57666b47e7b5c2292
Jonny Soth <janysoth@gmail.com>
2023-07-08 05:31:10 -0500

    Create  calculator result div.

--- Vue.js/vue-todo/src/views/CalculatorApp.vue
+++ Vue.js/vue-todo/src/views/CalculatorApp.vue
@@ -4,12 +4,31 @@
     style="max-width: 400px; margin: 50px auto; background: #234"
   >
     <!-- Calculator Result -->
+    <div
+      class="w-full rounded m-1 p-3 text-right lead font-weight-bold text-white bg-vue-dark"
+    >
+      {{ calculatorValue }}
+    </div>
   </div>
 </template>
 
-<script setup>
+<script>
+export default {
+  name: "Calculator",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      calculatorValue: "0",
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.bg-vue-dark {
+  background: #cad5e2;
+}
 </style>
 
