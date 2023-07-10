27a7f682c7ec519711653b1cfb1dc18013f29528
Jonny Soth <janysoth@gmail.com>
2023-07-10 00:05:35 -0500

    Finish the useCalculate and useKeyboard TypeScript files.

--- Vue.js/vue-todo/src/components/Caluculator/TypeScript/useCalculate.ts
+++ Vue.js/vue-todo/src/components/Caluculator/TypeScript/useCalculate.ts
@@ -1,4 +1,6 @@
 import { ref, readonly } from "vue";
+
+// Import the Arrays from the constants.js file
 import { OPERATORS, DIGITS } from "./constants";
 
 export function useCalculate() {
@@ -6,24 +8,33 @@ export function useCalculate() {
   let error = ref(false);
   let clearOnNextDigit = ref(false);
 
+  // Checks if a given string is  a valid operator
+  // By checking if it exists in the 'OPERATOR' Array
   function isOperator(string: string) {
     return OPERATORS.includes(string);
   }
 
+  // Checks if a given string is a valid digit
+  // By checking if it exists in the 'DIGITS' Array
   function isDigit(string: string) {
     return DIGITS.includes(string);
   }
 
+
+// Removes the whitespaces from the input string
+// And checks if the last character is a valid operator
   function lastCharIsOperator(string: string) {
     const stringNormalized = string.replace(/\s/g, "");
     return isOperator(stringNormalized[stringNormalized.length - 1]);
   }
 
+  // Adds a digit and update to the memory
   function addDigit(digit: string) {
     if (!isDigit(digit)) {
       throw new Error("Invalid param, is not a valid digit");
     }
 
+    // Set the lastDigit to the last index of the memory
     const lastDigit = memory.value[memory.value.length - 1];
 
     if (lastDigit === "." && digit === ".") return;
@@ -35,6 +46,7 @@ export function useCalculate() {
     memory.value += `${digit}`;
   }
 
+  // Adds an operator to the memory
   function addOperator(operator: string) {
     if (!isOperator(operator)) {
       throw new Error("Invalid param, is not a valid operator");
@@ -47,6 +59,9 @@ export function useCalculate() {
     memory.value += `${operator}`;
   }
 
+  // Calculates the result of the expresson stored in the memory
+  // Removes trailing operators if present
+  // Evaluates the expression using 'eval()' and update the result to the memory
   function calculateResult() {
     if (!memory.value) return;
 
