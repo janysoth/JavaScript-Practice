242297deb9c2db3baddff87191d54c656957b2a9
Jonny Soth <janysoth@gmail.com>
2023-07-12 05:30:06 -0500

    Import SiteNavigation.vue into App.vue

---
+++ Vue.js/vue-weather-app/src/components/SiteNavigation.vue
@@ -0,0 +1,8 @@
+// To have this component present on every page
+// Import this component in the App.vue
+<template>
+  <div></div>
+</template>
+
+<script setup>
+</script>
--- Vue.js/vue-weather-app/tailwind.config.js
+++ Vue.js/vue-weather-app/tailwind.config.js
@@ -1,28 +1,23 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      // Defind Custom Colors
-      color: {
+      colors: {
         "weather-primary": "#00668A",
-        "weather-secondary": "#004E71"
-      }
+        "weather-secondary": "#004E71",
+      },
     },
     fontFamily: {
       Roboto: ["Roboto, sans-serif"],
     },
-    // Add a container to avoid adding individually
     container: {
-      padding: "2rem", // Left & Right
-      center: true, // Similiar to margin-auto
+      padding: "2rem",
+      center: true,
     },
-    // Change the default sizes
     screens: {
       sm: "640px",
       md: "768px",
     },
   },
   plugins: [],
-}
-
+};
