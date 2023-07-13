d4cc444eece931c7026f906dc74395f8e9acc210
Jonny Soth <janysoth@gmail.com>
2023-07-13 05:12:41 -0500

    Add router link for the main header route to the home screen.

--- Vue.js/vue-weather-app/src/components/SiteNavigation.vue
+++ Vue.js/vue-weather-app/src/components/SiteNavigation.vue
@@ -1,8 +1,26 @@
 // To have this component present on every page
 // Import this component in the App.vue
 <template>
-  <div></div>
+  <header class="sticky top-0 bg-weather-primary shadow-lg">
+    <nav
+      class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
+    >
+      <!-- Set the header to the RouterLink to navigate back to the home screen -->
+      <!-- the name is the route name not the path -->
+      <RouterLink :to="{ name: 'home' }">
+        <div class="flex items-center gap-3 flex-1">
+          <i class="fa-solid fa-sun text-2xl"></i>
+          <p class="text-2xl">The Local Weather</p>
+        </div>
+      </RouterLink>
+
+      <div class="flex gap-3">
+        <i class="fa-solid fa-circle-info"></i>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script setup>
+import { RouterLink } from "vue-router";
 </script>
