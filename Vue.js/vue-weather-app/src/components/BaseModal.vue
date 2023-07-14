589b9b810db90fd65c97ca29b7821f7aa66e6e70
Jonny Soth <janysoth@gmail.com>
2023-07-14 05:36:06 -0500

    Create BaseModal.vue

--- Vue.js/vue-weather-app/src/components/BaseModal.vue
+++ Vue.js/vue-weather-app/src/components/BaseModal.vue
@@ -1,11 +1,18 @@
 <template>
   <div
+    v-show="modalActive"
     class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
   >
     <!-- Use slot for reusable and dynamic changes -->
-    <div class="p-4 bg-white self-start mt-32 max-w-screen-md">
+    <div
+      v-if="modalActive"
+      class="p-4 bg-white self-start mt-32 max-w-screen-md"
+    >
       <slot />
-      <button class="text-white mt-8 bg-weather-primary py-2 px-6">
+      <button
+        class="text-white mt-8 bg-weather-primary py-2 px-6"
+        @click="$emit('close-modal')"
+      >
         Close
       </button>
     </div>
@@ -13,6 +20,14 @@
 </template>
 
 <script setup>
+defineEmits(["close-modal"]);
+
+defineProps({
+  modalActive: {
+    type: Boolean,
+    default: false,
+  },
+});
 </script>
 
 <style lang="scss" scoped>
--- Vue.js/vue-weather-app/src/components/SiteNavigation.vue
+++ Vue.js/vue-weather-app/src/components/SiteNavigation.vue
@@ -1,4 +1,5 @@
-// To have this component present on every page
+// To have this 
+  components: { BaseModal },component present on every page
 // Import this component in the App.vue
 <template>
   <header class="sticky top-0 bg-weather-primary shadow-lg">
@@ -17,15 +18,83 @@
       <div class="flex gap-3 flex-1 justify-end">
         <i
           class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+          @click="toggleModal"
         ></i>
         <i
           class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"
         ></i>
       </div>
+
+      <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
+        <div class="text-black">
+          <h1 class="text-2xl mb-1">About:</h1>
+          <p class="mb-4">
+            The Local Weather allows you to track the current and future weather
+            of cities of your choosing.
+          </p>
+          <h2 class="text-2xl">How it works:</h2>
+          <ol class="list-decimal list-inside mb-4">
+            <li>
+              Search for your city by entering the name into the search bar.
+            </li>
+            <li>
+              Select a city within the results, this will take you to the
+              current weather for your selection.
+            </li>
+            <li>
+              Track the city by clicking on the "+" icon in the top right. This
+              will save the city to view at a later time on the home page.
+            </li>
+          </ol>
+
+          <h2 class="text-2xl">Removing a city</h2>
+          <p>
+            If you no longer wish to track a city, simply select the city within
+            the home page. At the bottom of the page, there will be am option to
+            delete the city.
+          </p>
+        </div>
+      </BaseModal>
     </nav>
   </header>
 </template>
 
 <script setup>
 import { RouterLink } from "vue-router";
+import { ref } from "vue";
+import BaseModal from "./BaseModal.vue";
+
+const modalActive = ref(null);
+const toggleModal = () => {
+  modalActive.value = !modalActive.value;
+};
 </script>
+
+<style scoped>
+.modal-outer-enter-active,
+.modal-outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-outer-enter-from,
+.modal-outer-leave-to {
+  opacity: 0;
+}
+
+.modal-inner-enter-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+
+.modal-inner-leave-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-inner-enter-from {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.modal-inner-leave-to {
+  transform: scale(0.8);
+}
+</style>
