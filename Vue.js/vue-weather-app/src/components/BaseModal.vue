092446776ee4d6ce8f33bde68e5f7e2e9b5a92ab
Jonny Soth <janysoth@gmail.com>
2023-07-14 05:11:18 -0500

    Create BaseModal.vue

---
+++ Vue.js/vue-weather-app/src/components/BaseModal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+  >
+    <!-- Use slot for reusable and dynamic changes -->
+    <div class="p-4 bg-white self-start mt-32 max-w-screen-md">
+      <slot />
+      <button class="text-white mt-8 bg-weather-primary py-2 px-6">
+        Close
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
