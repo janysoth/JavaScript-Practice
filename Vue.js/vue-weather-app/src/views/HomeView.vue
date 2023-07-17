3c13b85e3f206a69bad636994d60230bb54500e1
Jonny Vorn Soth <janysoth@gmail.comm>
2023-07-17 02:38:49 -0500

    Test Commit

--- Vue.js/vue-weather-app/src/views/HomeView.vue
+++ Vue.js/vue-weather-app/src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <div class="pt-4 mb-8 relative">
 
       <!-- Use v-model to capture the variable below -->
-      <input type="text" v-model="searchQuery" @input="getSearchResults" placeholder="Search For A City or State Here..."
+      <input type="text" v-model="searchQuery" @input="getSearchResults" placeholder="Search For A City..."
         class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]" />
 
       <!-- To show mapboxSearchResults on the page -->
