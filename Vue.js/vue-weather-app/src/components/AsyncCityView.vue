03345a90ca9fe07cd6799ef058e0a73b626ecf5f
janysoth <janysoth@gmail.com>
2023-07-23 00:44:05 -0500

    Add temp description and the temp.

--- Vue.js/vue-weather-app/src/components/AsyncCityView.vue
+++ Vue.js/vue-weather-app/src/components/AsyncCityView.vue
@@ -65,6 +65,11 @@
               })
               }}
             </p>
+            <img class="w-auto h-[50px] object-cover" :src="`http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`
+              " alt="" />
+            <p class="text-xl">
+              {{ Math.round(hourData.temp) }}&deg;F
+            </p>
           </div>
         </div>
       </div>
