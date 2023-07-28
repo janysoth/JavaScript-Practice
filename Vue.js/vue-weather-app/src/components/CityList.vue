c3d0ca59924ed2a098db439f755424a3dc4515c7
Jonny Soth <janysoth@gmail.com>
2023-07-28 04:58:08 -0500

    Moved codes from CityCard to CityList.

--- Vue.js/vue-weather-app/src/components/CityList.vue
+++ Vue.js/vue-weather-app/src/components/CityList.vue
@@ -3,5 +3,37 @@
 </template>
 
 <script setup>
+import axios from "axios";
+import { ref } from "vue";
+
+const savedCities = ref([]);
+
+// To Retrieve the Cities
+const getCities = async () => {
+  if (localStorage.getItem("savedCities")) {
+    savedCities.value = JSON.parse(localStorage.getItem("savedCities"));
+
+    const requests = [];
+
+    savedCities.value.forEach((city) => {
+      requests.push(
+        axios.get(
+          `https://api.openweathermap.org/data/3.0/weather?lat=${city.coords.lat}&lon=${city.coords.lng}&appid=d5d4f9caa31dc0bc654d876b1d629d6e&units=imperial`
+        )
+      );
+    });
+
+    // To Resolve all of the requests before moving on
+    const weatherData = await Promise.all(requests);
+
+    // Take the weaterData from the API and match with the savedCities
+    weatherData.forEach((value, index) => {
+      savedCities.value[index].weather = value.data;
+    });
+  }
+};
+
+// To Invoke the function
+await getCities();
 </script>
 
