fb2f1bfc8adef3b33835ec5c40bafd82f445e8aa
Jonny Soth <janysoth@gmail.com>
2023-08-02 05:20:10 -0500

    Add name for each tab.

--- Vue.js/vue-weather-app/src/router/index.js
+++ Vue.js/vue-weather-app/src/router/index.js
@@ -8,17 +8,32 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      // Add title to each route
+      meta: {
+        title: "Home",
+      }
     },
     {
       // /:state/:city are params
       // Use /: and param for the path
       path: '/weather/:state/:city',
       name: 'cityView',
-      component: CityView
+      component: CityView,
+      // Add title to each route
+      meta: {
+        title: "Weather",
+      }
     },
 
   ]
+});
+
+// To Set Tab Title
+// next () to load the rest of the page content
+router.beforeEach((to, from, next) => {
+  document.title = `${to.params.state ? `${to.params.city}, ${to.params.state}` : to.matched[0].meta.title} | The Local Weather`;
+  next();
 })
 
 export default router
