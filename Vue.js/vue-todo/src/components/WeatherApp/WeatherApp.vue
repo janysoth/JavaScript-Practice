acddea6806f007f1bb588fda8362538b9d0a6779
Jonny Soth <janysoth@gmail.com>
2023-08-03 05:28:50 -0500

    Add header and navigation bar in SiteNavigation.vue.

--- Vue.js/vue-todo/src/components/WeatherApp/WeatherApp.vue
+++ Vue.js/vue-todo/src/components/WeatherApp/WeatherApp.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>Hello</div>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation />
+    <RouterView />
+  </div>
 </template>
 
 <script setup>
+import SiteNavigation from "./SiteNavigation.vue";
 </script>
 
 <style lang="scss" scoped>
