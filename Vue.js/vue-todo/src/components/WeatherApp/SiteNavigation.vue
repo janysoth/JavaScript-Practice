03cc0db779eca512dd0d021facc79aab47aa7220
janysoth <janysoth@gmail.com>
2023-08-07 00:48:39 -0500

    Finish city search mark up.

--- Vue.js/vue-todo/src/components/WeatherApp/SiteNavigation.vue
+++ Vue.js/vue-todo/src/components/WeatherApp/SiteNavigation.vue
@@ -1,8 +1,8 @@
 <template>
-  <header class="sticky top-0 bg-cambodia-red shadow-lg">
+  <header class="sticky top-0 bg-cambodia-blue shadow-lg">
     <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
       <!-- Weather Icon and Header for the WeatherApp -->
-      <RouterLink :to="{ name: 'WeatherApp' }" class="hover:text-cambodia-blue duration-150 cursor-pointer">
+      <RouterLink :to="{ name: 'WeatherApp' }" class="hover:text-cambodia-red duration-150 cursor-pointer">
         <div class="flex items-center gap-3">
           <i class="fa-solid fa-sun text-xl"> </i>
           <p class="text-xl flex items-center">Your Local Weather</p>
@@ -12,9 +12,9 @@
 
       <!-- BaseModal and Plus icon -->
       <div class="flex gap-3 flex-1 justify-end">
-        <i class="fa-solid fa-circle-info text-xl hover:text-cambodia-blue duration-150 cursor-pointer"
+        <i class="fa-solid fa-circle-info text-xl hover:text-cambodia-red duration-150 cursor-pointer"
           @click="toggleModal"></i>
-        <i class="fa-solid fa-plus text-xl hover:text-cambodia-blue duration-150 cursor-pointer"></i>
+        <i class="fa-solid fa-plus text-xl hover:text-cambodia-red duration-150 cursor-pointer"></i>
       </div>
       <!---->
 
--- Vue.js/vue-todo/src/components/WeatherApp/WeatherApp.vue
+++ Vue.js/vue-todo/src/components/WeatherApp/WeatherApp.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="flex flex-col min-h-screen font-Roboto bg-cambodia-blue">
+  <div class="flex flex-col min-h-screen font-Roboto bg-cambodia-red">
     <SiteNavigation />
     <RouterView />
+    <HomeView />
   </div>
 </template>
 
 <script setup>
+import HomeView from "./HomeView.vue";
 import SiteNavigation from "./SiteNavigation.vue";
 </script>
 
