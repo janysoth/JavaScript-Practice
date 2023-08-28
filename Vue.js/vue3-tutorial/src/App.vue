d0da3d144ad56635fda96305c016965e6463c22f
janysoth <janysoth@gmail.com>
2023-08-28 00:01:52 -0500

    Create FamilyMembers component and import it to App.vue.

--- Vue.js/vue3-tutorial/src/App.vue
+++ Vue.js/vue3-tutorial/src/App.vue
@@ -1,75 +1,15 @@
 <template>
   <AppHeader />
   <div class="w-full flex">
-    <div class="m-auto">
-      <h1 class="text-3xl font-bold text-red-700 text-center">Family Members:</h1>
-      <ul class="mt-2">
-        <li class="flex justify-between items-center" v-for="(member, index) in familyMembers" :key="member.name">
-          <div class="flex items-center">
-            <span>{{ member.name }}</span>
-          </div>
-          <button @click="remove(index)">x</button>
-        </li>
-      </ul>
-
-      <form class="mt-10" @submit.prevent="addNewMember">
-        <input class="border rounded mr-1" v-model="newMember" placeholder="Add new member here..." />
-        <button class="border rounded bg-gradient-to-r from-red-700 to-pink-500 text-white p-1" type="submit">Add
-          Member</button>
-      </form>
-    </div>
+    <FamilyMembers />
   </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue';
+import FamilyMembers from './components/FamilyMembers.vue';
 
 export default {
-  components: { AppHeader },
-  computed: {
-    memberCount() {
-      return this.familyMembers.length;
-    },
-    fullName: {
-      get() {
-        return `Full Name is: ${this.fname} ${this.lname}`;
-      },
-      set(fullName) {
-        alert(fullName);
-      },
-    },
-  },
-  data() {
-    return {
-      newMember: "",
-      fname: "Jonny Vorn",
-      lname: "Soth",
-      familyMembers: [
-        { name: "Jonny Vorn Soth" },
-        { name: "Saominea Soth" },
-        { name: "Manika Soth" },
-        { name: "Samantta Soth" },
-        { name: "Jaccika Soth" },
-      ],
-    };
-  },
-  // Cannot use arrow function as it does not hold this
-  // Use this for any variables in the data()
-  // unshift: add to the front of the array
-  methods: {
-    addNewMember() {
-      if (this.newMember !== "") {
-        this.familyMembers.push({ name: this.newMember });
-        this.newMember = "";
-      }
-    },
-    setFullName() {
-      this.fullName = this.newMember;
-    },
-    remove(index) {
-      this.familyMembers = this.familyMembers.filter((member, i) => i !== index);
-    },
-  },
-
+  components: { AppHeader, FamilyMembers },
 };
 </script>
