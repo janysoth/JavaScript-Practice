1a11fda60c86777feaf97e2073b5b58ab82f9d6a
Jonny Soth <janysoth@gmail.com>
2023-08-30 05:21:28 -0500

    Create startDate() to display the  first day of the month.

--- Vue.js/vue3-tutorial/src/components/CalendarApp.vue
+++ Vue.js/vue3-tutorial/src/components/CalendarApp.vue
@@ -2,14 +2,15 @@
     <div class="m-auto">
         <h1 class="text-2xl my-2 text-center">Vue Calendar</h1>
         <section class="mx-2 flex justify-between">
-            <h2 class="font-bold">{{ currentMonth }}</h2>
+            <h2 class="font-bold">{{ currentMonthName }}</h2>
             <h2 class="font-bold">{{ currentYear }}</h2>
         </section>
         <section class="flex my-2">
             <p class="text-center  w-[14.28%]" v-for="day in days" :key="day">{{ day }}</p>
         </section>
         <section class="flex flex-wrap">
-            <p class="text-center w-[14.28%]" v-for="num in 30" :key="num">{{ num }}</p>
+            <p class="text-center w-[14.28%]" v-for="num in startDate()" :key="num"></p>
+            <p class="text-center w-[14.28%]" v-for="num in daysInMonth(currentYear, currentMonth)" :key="num">{{ num }}</p>
         </section>
     </div>
 </template>
@@ -18,11 +19,24 @@
 export default {
     data() {
         return {
-            currentMonth: new Date().toLocaleString('default', { month: "long" }),
+            currentMonth: new Date().getMonth() + 1,
+            currentMonthName: new Date().toLocaleString('default', { month: "long" }),
             currentYear: new Date().getFullYear(),
             days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
         }
     },
+
+    methods: {
+        daysInMonth(year, month) {
+
+            const lastDay = new Date(year, month, 0).getDate();
+            return lastDay;
+        },
+
+        startDate() {
+            return new Date(this.currentYear, this.currentMonth - 1, 1).getDay();
+        },
+    }
 }
 </script>
 
