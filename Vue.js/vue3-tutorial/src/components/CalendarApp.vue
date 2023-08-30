a148aedc1907cfe7f0df3c37f83512af5890f685
Jonny Soth <janysoth@gmail.com>
2023-08-30 04:54:05 -0500

    Display current Month an year.

--- Vue.js/vue3-tutorial/src/components/CalendarApp.vue
+++ Vue.js/vue3-tutorial/src/components/CalendarApp.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="m-auto">
         <h1 class="text-2xl my-2 text-center">Vue Calendar</h1>
+        <section class="mx-2 flex justify-between">
+            <h2 class="font-bold">{{ currentMonth }}</h2>
+            <h2 class="font-bold">{{ currentYear }}</h2>
+        </section>
         <section class="flex my-2">
             <p class="text-center  w-[14.28%]" v-for="day in days" :key="day">{{ day }}</p>
         </section>
@@ -14,6 +18,8 @@
 export default {
     data() {
         return {
+            currentMonth: new Date().toLocaleString('default', { month: "long" }),
+            currentYear: new Date().getFullYear(),
             days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
         }
     },
