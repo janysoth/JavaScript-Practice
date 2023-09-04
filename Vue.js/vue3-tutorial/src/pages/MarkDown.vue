ef01c34a4dd3ccfeb32d890460e02c264bdd03bd
janysoth <janysoth@gmail.com>
2023-09-04 00:02:46 -0500

    Create markdown up and create debounce.js using mixins.

--- Vue.js/vue3-tutorial/src/pages/MarkDown.vue
+++ Vue.js/vue3-tutorial/src/pages/MarkDown.vue
@@ -1,9 +1,50 @@
 <template>
-    MarkDown
+    <div class="flex flex-wrap w-full">
+        <h1 class="w-full text-center text-3xl my-4">Markdown App</h1>
+        <section class="flex m-auto w-10/12 h-screen">
+            <article class="w-1/2 border">
+                <textarea class="w-full h-full" :value="text" @input="update"></textarea>
+            </article>
+            <article class="w-1/2 border bg-gray-100" v-html="markedText"></article>
+        </section>
+    </div>
 </template>
 
 <script>
+import { marked } from "marked";
+import debounce from '../utilities/mixins/debounce';
+
 export default {
+    beforeCreate() {
+
+    },
+
+    created() {
+
+    },
+
+    mixins: [debounce],
+
+    data() {
+        return {
+            text: "",
+        };
+    },
+
+    computed: {
+        markedText() {
+            return marked(this.text);
+        }
+    },
+
+    methods: {
+        // To Update when the user done typing:
+        update(e) {
+            const task = () => this.text = e.target.value;
+            this.debounce(task, 500);
+        },
+    }
+
 
 };
 </script>
