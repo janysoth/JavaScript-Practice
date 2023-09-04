c8ddcaab59c1136bcb2157af77566ec751d4652f
janysoth <janysoth@gmail.com>
2023-09-03 21:52:17 -0500

    Create list array to store all of the paths in the header.

--- Vue.js/vue3-tutorial/src/components/AppHeader.vue
+++ Vue.js/vue3-tutorial/src/components/AppHeader.vue
@@ -1,14 +1,23 @@
 <template>
     <nav class="w-full bg-gradient-to-r from-blue-800 to-blue-600 text-white px-4 py-2">
-        <router-link class="mx-2" to="/">Home</router-link>
-        <router-link class="mx-2" to="/family-members">Family Members</router-link>
-        <router-link class="mx-2" to="/calendar">Calendar</router-link>
+        <router-link v-for="item in list" :key="item.to" class="mx-2" :to="item.to">{{ item.title }}</router-link>
+        <!-- <router-link class="mx-2" to="/calendar">Calendar</router-link>
+        <router-link class="mx-2" to="/markdown">Mark Down</router-link> -->
     </nav>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            list: [
+                { title: "Home", to: "/" },
+                { title: "Family Members", to: "/family-members" },
+                { title: "Calendar", to: "/calendar" },
+                { title: "Mark Down", to: "/markdown" },
+            ],
+        };
+    }
 };
 </script>
 
--- Vue.js/vue3-tutorial/src/router.js
+++ Vue.js/vue3-tutorial/src/router.js
@@ -1,13 +1,15 @@
 /* eslint-disable */
 import { createRouter, createWebHistory } from "vue-router";
-import FamilyMembers from './pages/FamilyMembers'
-import CalendarApp from './pages/CalendarApp'
-import HomePage from './pages/HomePage'
+import FamilyMembers from './pages/FamilyMembers';
+import CalendarApp from './pages/CalendarApp';
+import HomePage from './pages/HomePage';
+import MarkDown from './pages/MarkDown';
 
 const routes = [
     { path: '/', component: HomePage },
     { path: '/family-members', component: FamilyMembers },
     { path: '/calendar', component: CalendarApp },
+    { path: '/markdown', component: MarkDown },
 ];
 
 const router = createRouter({
