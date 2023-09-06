ad000ae4108035cfd6a546e115c1b0ca7c9407b4
Jonny Soth <janysoth@gmail.com>
2023-09-06 05:28:24 -0500

    Learn about transition in Vue.js

--- Vue.js/vue3-tutorial/src/pages/SliderApp.vue
+++ Vue.js/vue3-tutorial/src/pages/SliderApp.vue
@@ -1,8 +1,26 @@
 <template>
-    <div class="flex flex-wrap w-full">
+    <div class="flex flex-wrap w-full relative">
         <div class="absolute w-full" v-for="(color, index) in sliders" :key="color">
-            <div v-if="currentSlide == index" :class="color" style="height:350px"></div>
+            <transition name="fade">
+                <div v-if="currentSlide == index" :class="color" style="height:350px"></div>
+            </transition>
         </div>
+
+        <div class="w-full" style="height:340px">
+            <div class="absolute  bottom-0 flex justify-center w-full">
+                <div v-for="(slider, index) in sliders" :key="slider" class="w-4 h-4 rounded-full mx-2 cursor-pointer"
+                    :class="currentSlide == index ? 'bg-gray-600' : 'bg-gray-300'"></div>
+            </div>
+        </div>
+
+        <!-- <div class="my-10 flex w-full">
+            <div class="m-auto">
+                <transition name="fade">
+                    <h1 v-if="isTitleShowing">Slider Carousels</h1>
+                </transition>
+                <button @click="isTitleShowing = !isTitleShowing" class="px-2 rounded border">Toggle Text</button>
+            </div>
+        </div> -->
     </div>
 </template>
 
@@ -12,7 +30,8 @@ export default {
         return {
             currentSlide: 0,
             interval: "",
-            sliders: ['bg-teal-600', 'bg-blue-600', 'bg-yellow-600']
+            sliders: ['bg-teal-600', 'bg-blue-600', 'bg-yellow-600'],
+            isTitleShowing: true,
         };
     },
 
@@ -29,4 +48,19 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+    transition: all 1s ease;
+}
+
+.fade-enter-from {
+    opacity: 0;
+    transform: translateX(-100%);
+}
+
+.fade-leave-to {
+    opacity: 0;
+    transform: translateX(100%);
+}
+</style>
