2ac10be88fa16544ac2ce3e8537a8c2b69283126
Jonny Soth <janysoth@gmail.com>
2023-09-07 05:26:28 -0500

    Create LoginModal.

--- Vue.js/vue3-tutorial/src/App.vue
+++ Vue.js/vue3-tutorial/src/App.vue
@@ -4,15 +4,15 @@
     <div class="w-full flex">
       <router-view></router-view>
     </div>
+    <LoginModal />
   </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue';
-// import CalendarApp from './components/CalendarApp.vue';
-// import FamilyMembers from './components/FamilyMembers.vue';
+import LoginModal from './components/LoginModal.vue';
 
 export default {
-  components: { AppHeader },
+  components: { AppHeader, LoginModal },
 };
 </script>
