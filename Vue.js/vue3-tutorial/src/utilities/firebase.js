8479026ccaf22e4a1e01c68c988127afefb2b4c0
Jonny Soth <janysoth@gmail.com>
2023-09-08 05:32:35 -0500

    Learn about firebase.

---
+++ Vue.js/vue3-tutorial/src/utilities/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAN1myM9SyAE7u-ywI7WDh2Z2OTgztF2N4",
+  authDomain: "vue-full-course-3f703.firebaseapp.com",
+  projectId: "vue-full-course-3f703",
+  storageBucket: "vue-full-course-3f703.appspot.com",
+  messagingSenderId: "580420744869",
+  appId: "1:580420744869:web:042ce97e23bb65386f3f4c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
