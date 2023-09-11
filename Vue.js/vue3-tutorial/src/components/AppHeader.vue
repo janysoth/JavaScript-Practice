1e819bc1b6c078574f8d51d09b437c1ab2dcb436
janysoth <janysoth@gmail.com>
2023-09-11 08:00:03 -0500

    Move login/logout to the right of the app header and display user's email once logged in.

--- Vue.js/vue3-tutorial/src/components/AppHeader.vue
+++ Vue.js/vue3-tutorial/src/components/AppHeader.vue
@@ -6,8 +6,9 @@
                 <router-link v-for="item in list" :key="item.to" class="mx-2" :to="item.to">{{ item.title }}</router-link>
             </div>
             <div>
-                <button class="mx-2" @click="$emit('open-login-modal')">Login</button>
-                <button class="mx-2" @click="logout">Logout</button>
+                <span v-if="user" class="mx-2">{{ user.email }}</span>
+                <button v-if="!user" class="mx-2" @click="$emit('open-login-modal')">Login</button>
+                <button v-else class="mx-2" @click="logout">Logout</button>
             </div>
         </nav>
     </div>
@@ -26,15 +27,19 @@ export default {
                 { title: "Mark Down", to: "/markdown" },
                 { title: "Slider", to: "/slider" },
             ],
+            user: null, // Initialize user to null
         };
     },
-
+    created() {
+        // Check the user's authentication state when the component is created
+        firebase.auth().onAuthStateChanged((user) => {
+            this.user = user; // Update the user data
+        });
+    },
     methods: {
         logout() {
             firebase.auth().signOut().then(res => { console.log(res); }).catch((e) => { console.log(e); });
         },
-    }
+    },
 };
 </script>
-
-<style></style>
