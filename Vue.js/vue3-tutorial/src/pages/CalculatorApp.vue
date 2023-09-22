6d806d94e678edb0f4fbb158855ccb32948094c8
Jonny Soth <janysoth@gmail.com>
2023-09-22 00:02:08 -0500

    Create functions for the calculator app.

--- Vue.js/vue3-tutorial/src/pages/CalculatorApp.vue
+++ Vue.js/vue3-tutorial/src/pages/CalculatorApp.vue
@@ -2,25 +2,66 @@
   <section class="flex w-full">
     <div class="m-auto">
       <h1 class="text-2xl text-center">Calculator</h1>
-      <div class="my-10 grid grid-cols-3 gap-1">
-        <button class="p-2 border rounded  shadow w-10 h-10">1</button>
-        <button class="p-2 border rounded  shadow w-10 h-10">2</button>
-        <button class="p-2 border rounded  shadow w-10 h-10">3</button>
-        <button class="p-2 border rounded  shadow w-10 h-10">4</button>
-        <button class="p-2 border rounded  shadow w-10 h-10">5</button>
-        <button class="p-2 border rounded  shadow w-10 h-10">6</button>
-        <button class="p-2 border rounded  shadow w-10 h-10">7</button>
-        <button class="p-2 border rounded  shadow w-10 h-10">8</button>
-        <button class="p-2 border rounded  shadow w-10 h-10">9</button>
-        <button class="p-2 border rounded  shadow h-10 col-span-2">0</button>
-        <button class="p-2 border rounded  shadow w-10 h-10">=</button>
+      <p class="text-3xl  text-right mt-10 mb-2  w-32 h-10 overflow-x-scroll" style="direction:rtl">{{ calculation }}</p>
+      <div class="grid grid-cols-4 gap-1">
+        <button @click="pressed('1')" class="p-2 border rounded  shadow w-10 h-10">1</button>
+        <button @click="pressed('2')" class="p-2 border rounded  shadow w-10 h-10">2</button>
+        <button @click="pressed('3')" class="p-2 border rounded  shadow w-10 h-10">3</button>
+        <button @click="pressed('+')" class="p-2 border rounded  shadow w-10 h-10">+</button>
+        <button @click="pressed('4')" class="p-2 border rounded  shadow w-10 h-10">4</button>
+        <button @click="pressed('5')" class="p-2 border rounded  shadow w-10 h-10">5</button>
+        <button @click="pressed('6')" class="p-2 border rounded  shadow w-10 h-10">6</button>
+        <button @click="pressed('-')" class="p-2 border rounded  shadow w-10 h-10">-</button>
+        <button @click="pressed('7')" class="p-2 border rounded  shadow w-10 h-10">7</button>
+        <button @click="pressed('8')" class="p-2 border rounded  shadow w-10 h-10">8</button>
+        <button @click="pressed('9')" class="p-2 border rounded  shadow w-10 h-10">9</button>
+        <button @click="pressed('*')" class="p-2 border rounded  shadow w-10 h-10">*</button>
+        <button @click="pressed('C')" class="p-2 border rounded  shadow w-10 h-10 ">C</button>
+        <button @click="pressed('0')" class="p-2 border rounded  shadow w-10 h-10 ">0</button>
+        <button @click="pressed('=')" class="p-2 border rounded  shadow w-10 h-10">=</button>
+        <button @click="pressed('/')" class="p-2 border rounded  shadow w-10 h-10">/</button>
       </div>
     </div>
   </section>
 </template>
 
 <script>
+
+import { ref } from 'vue';
+
 export default {
+  setup() {
+    const currentNum = ref("");
+    const operations = ['+', '-', '*', '/'];
+    const previousNum = ref("");
+    const selectedOperation = ref("");
+
+    function pressed(value) {
+      if (value === "=") calculate();
+      else if (value === "C") clear();
+      else if (operations.includes(value)) applyOperation(value);
+      else appendNumber(value);
+    }
+
+    function appendNumber(value) {
+      currentNum.value = currentNum.value + value;
+    }
+
+    function applyOperation(value) {
+      currentNum.value = previousNum.value;
+      currentNum.value = "";
+    }
+
+    function calculate() {
+
+    }
+
+    function clear() {
+      currentNum.value = '';
+    }
+
+    return { calculation: currentNum, pressed, calculate, applyOperation, appendNumber, clear };
+  },
 
 }
 </script>
