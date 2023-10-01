138178d4a0bcb49d3d6b497b2621f0b5bcd7c5b3
Jonny Soth <janysoth@gmail.com>
2023-10-01 02:22:49 -0500

    Create another version of calculator app.

--- Vue.js/vue3-tutorial/src/App.vue
+++ Vue.js/vue3-tutorial/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class>
     <AppHeader :isLoggedIn="isLoggedIn" @open-login-modal="isLoginOpen = true" />
     <div class="w-full flex">
       <router-view></router-view>
--- Vue.js/vue3-tutorial/src/pages/CalculatorApp.vue
+++ Vue.js/vue3-tutorial/src/pages/CalculatorApp.vue
@@ -1,129 +1,260 @@
 <template>
-  <section class="flex justify-center items-center w-full">
-    <div class="mx-auto w-90 max-w-[400px] bg-lightcoral rounded-[20px] overflow-hidden border-3 border-goldenrod">
-      <h1 class="text-2xl text-center">Calculator</h1>
-      <div class="bg-whitesmoke text-right text-30px relative">
-        <p class="text-3xl text-right mt-5 w-00 h-10 px-2 py-2 bg-white text-gray-800 rounded-lg shadow-md"
-          style="direction: rtl">
-          {{ currentNum }}</p>
-        <div class="w-40 h-10 text-left">
-          <small v-if="selectedOperation" class="text-gray-600">
-            {{ prevNum }} {{ selectedOperation }} {{ currentNum }}
-          </small>
-        </div>
-      </div>
-      <div class="grid grid-cols-4 gap-1 w-[300px] h-[300px] textxl">
-        <CalculatorButton @click="pressed('C')" color="red" class="hover:opacity-60" button-text="C" />
-        <CalculatorButton @click="pressed('CE')" color="red" class="hover:opacity-60" button-text="CE" />
-        <CalculatorButton @click="deleteLast" color="red" class="hover:opacity-60" button-text="←" />
-        <CalculatorButton @click="pressed('/')" color="red" class="hover:opacity-60" button-text="/" />
-        <CalculatorButton @click="pressed('1')" color="blue" class="hover:opacity-60" button-text="1" />
-        <CalculatorButton @click="pressed('2')" color="blue" class="hover:opacity-60" button-text="2" />
-        <CalculatorButton @click="pressed('3')" color="blue" class="hover:opacity-60" button-text="3" />
-        <CalculatorButton @click="pressed('+')" color="red" class="hover:opacity-60" button-text="+" />
-        <CalculatorButton @click="pressed('4')" color="blue" class="hover:opacity-60" button-text="4" />
-        <CalculatorButton @click="pressed('5')" color="blue" class="hover:opacity-60" button-text="5" />
-        <CalculatorButton @click="pressed('6')" color="blue" class="hover:opacity-60" button-text="6" />
-        <CalculatorButton @click="pressed('-')" color="red" class="hover:opacity-60" button-text="-" />
-        <CalculatorButton @click="pressed('7')" color="blue" class="hover:opacity-60" button-text="7" />
-        <CalculatorButton @click="pressed('8')" color="blue" class="hover:opacity-60" button-text="8" />
-        <CalculatorButton @click="pressed('9')" color="blue" class="hover:opacity-60" button-text="9" />
-        <CalculatorButton @click="pressed('*')" color="red" class="hover:opacity-60" button-text="x" />
-        <CalculatorButton @click="pressed('.')" color="red" class="hover:opacity-60" button-text="." />
-        <CalculatorButton @click="pressed('0')" color="blue" class="hover:opacity-60" button-text="0" />
-        <CalculatorButton @click="pressed('=')" color="red" class="col-span-2 hover:opacity-60" button-text="=" />
-      </div>
-    </div>
-  </section>
+  <div class="calculator m-auto">
+    <table cellspacing="10">
+      <tr>
+        <td colspan="4">
+          <input type="text" v-model="result" disabled>
+        </td>
+      </tr>
+      <tr>
+        <td class="button dark" @click="clear">C</td>
+        <td class="button dark" @click="invert">+/-</td>
+        <td class="button dark" @click="percent">%</td>
+        <td class="button orange" @click="setOperator('/')">/</td>
+      </tr>
+      <tr>
+        <td class="button grey" @click="addNumber(7)">7</td>
+        <td class="button grey" @click="addNumber(8)">8</td>
+        <td class="button grey" @click="addNumber(9)">9</td>
+        <td class="button orange" @click="setOperator('*')">*</td>
+      </tr>
+      <tr>
+        <td class="button grey" @click="addNumber(4)">4</td>
+        <td class="button grey" @click="addNumber(5)">5</td>
+        <td class="button grey" @click="addNumber(6)">6</td>
+        <td class="button orange" @click="setOperator('-')">-</td>
+      </tr>
+      <tr>
+        <td class="button grey" @click="addNumber(1)">1</td>
+        <td class="button grey" @click="addNumber(2)">2</td>
+        <td class="button grey" @click="addNumber(3)">3</td>
+        <td class="button orange" @click="setOperator('+')">+</td>
+      </tr>
+      <tr>
+        <td class="button-col2 grey" @click="addNumber(0)" colspan="2">0</td>
+        <td class="button grey" @click="addPoint">.</td>
+        <td class="button orange" @click="equal">=</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
-
 <script>
-
-import { onMounted, onUnmounted, ref } from 'vue';
-import CalculatorButton from '@/components/CalculatorApp/CalculatorButton.vue'
-
 export default {
-  components: {
-    CalculatorButton,
-  },
-  setup() {
-    const currentNum = ref("");
-    const operations = ['+', '-', '*', '/'];
-    const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-    const prevNum = ref("");
-    const selectedOperation = ref("");
-
-    function pressed(value) {
-      if (value === "=" || value === "Enter") calculate();
-      else if (value === "C") clear();
-      else if (operations.includes(value)) applyOperation(value);
-      else if (numbers.includes(value)) appendNumber(value);
+  data: function () {
+    return {
+      result: 0,
+      tmp_value: 0,
+      reset: false,
+      operator: undefined
     }
-
-    function appendNumber(value) {
-      // if (currentNum.value !== "")
-      //   clear();
-      currentNum.value = currentNum.value + value;
-    }
-
-    function applyOperation(value) {
-      // Before apply any operation, calculate firts:
-      calculate();
-      prevNum.value = currentNum.value;
-      currentNum.value = "";
-      selectedOperation.value = value;
-    }
-
-    // When the user click on the "="
-    function calculate() {
-      if (selectedOperation.value === '*') multiply();
-      else if (selectedOperation.value === '/') divide();
-      else if (selectedOperation.value === '-') subtract();
-      else if (selectedOperation.value === '+') sum();
-
-      prevNum.value = '';
-      selectedOperation.value = '';
-    }
-
-    function multiply() {
-      currentNum.value = prevNum.value * currentNum.value;
-    }
-    function divide() {
-      currentNum.value = prevNum.value / currentNum.value;
-    }
-    function subtract() {
-      currentNum.value = prevNum.value - currentNum.value;
-    }
-    function sum() {
-      currentNum.value = +prevNum.value + +currentNum.value;
-    }
-
-    const clear = (() => {
-      currentNum.value = '';
-      prevNum.value = '';
-      selectedOperation.value = '';
-    });
-
-    const deleteLast = (() => {
-      currentNum.value = currentNum.value.toString().slice(0, -1);
-    });
-
-
-
-    const handledKeydown = (e) => pressed(e.key);
-
-    onMounted(() =>
-      window.addEventListener('keydown', handledKeydown));
-
-    // To remove eventListener after leaving this page
-    onUnmounted(() =>
-      window.removeEventListener('keydown', handledKeydown));
-
-    return { currentNum, prevNum, pressed, calculate, applyOperation, appendNumber, clear, selectedOperation, multiply, divide, subtract, sum, deleteLast };
   },
 
-};
+  mounted() {
+    // Register keyboard event listeners
+    window.addEventListener('keydown', this.handleKeyPress);
+  },
+
+  beforeMount() {
+    // Remove the event listener when the component is destroyed
+    window.removeEventListener('keydown', this.handleKeyPress);
+  },
+
+  methods: {
+
+    clear() {
+      this.result = 0;
+      this.tmp_value = 0;
+      this.operator = undefined;
+    },
+
+    invert() {
+      this.result *= -1;
+    },
+
+    percent() {
+      this.result /= 100;
+    },
+
+    addNumber(number) {
+      if (this.result == 0 || this.reset === true) {
+        this.result = '';
+        this.reset = false;
+      }
+
+      this.result += number.toString();
+    },
+    addPoint() {
+      if (!this.result.includes('.'))
+        this.result += '.';
+    },
+    setOperator(operator) {
+      if (this.tmp_value != 0)
+        this.calculate();
+
+      this.tmp_value = this.result;
+      this.operator = operator;
+      this.reset = true;
+    },
+    equal() {
+      this.calculate();
+      this.tmp_value = 0;
+      this.operator = undefined;
+    },
+    calculate() {
+      let value = 0;
+      let firstNum = parseFloat(this.tmp_value);
+      let secondNum = parseFloat(this.result);
+
+      switch (this.operator) {
+        case '+':
+          value = firstNum + secondNum;
+          break;
+        case '-':
+          value = firstNum - secondNum;
+          break;
+        case '*':
+          value = firstNum * secondNum;
+          break;
+        case '/':
+          value = firstNum / secondNum;
+      }
+
+      this.result = value.toString();
+    },
+
+    handleKeyPress(event) {
+      // Check if the key pressed is a numeric keypad key (0-9), a decimal point, an operator, or the backspace key
+      if (/^[0-9.]$/.test(event.key)) {
+        event.preventDefault(); // Prevent the default behavior of keypress
+        this.handleKeyboardInput(event.key);
+      } else if (/^[+\-*/]$/.test(event.key)) {
+        event.preventDefault();
+        this.handleKeyboardOperator(event.key);
+      } else if (event.key === 'Enter') {
+        // Handle Enter key as an equals sign
+        event.preventDefault();
+        this.equal();
+      } else if (event.key === 'Escape') {
+        // Handle Escape key as a clear operation
+        event.preventDefault();
+        this.clear();
+      } else if (event.key === 'Backspace') {
+        // Handle Backspace key to delete the last digit
+        event.preventDefault();
+        this.deleteLastDigit();
+      }
+    },
+
+    handleKeyboardInput(key) {
+      if (this.reset === true) {
+        this.result = ''; // Clear the result if it needs to be reset
+        this.reset = false;
+      }
+
+      if (key === '.') {
+        this.addPoint();
+      } else {
+        this.addNumber(key);
+      }
+    },
+
+    handleKeyboardOperator(operator) {
+      if (this.tmp_value != 0) {
+        this.calculate();
+      }
+
+      this.tmp_value = this.result;
+      this.operator = operator;
+      this.reset = true;
+    },
+
+    deleteLastDigit() {
+      if (this.result.length > 0) {
+        this.result = this.result.slice(0, -1); // Remove the last character
+      }
+    },
+  }
+}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.calculator {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 3rem;
+
+
+  table {
+    /* Add a border to the table */
+    border-radius: 10px;
+    /* Add rounded corners to the table */
+    background-color: black;
+    margin-bottom: 100px;
+  }
+
+  input {
+    display: block;
+    width: 400px;
+    /* Match the button width */
+    height: 75px;
+    padding: 5px 20px 0;
+    margin-bottom: 10px;
+    border: none;
+    border-radius: 10px;
+    background-color: red;
+    color: #fff;
+    font-size: 4rem;
+    text-align: right;
+  }
+
+  .button {
+    margin: 5px;
+    /* Add margin to create spacing between buttons */
+    border-radius: 40px;
+    width: 80px;
+    height: 80px;
+    text-align: center;
+    font-weight: bold;
+    cursor: pointer;
+  }
+
+  .button-col2 {
+    border-radius: 40px;
+    width: 160px;
+    height: 80px;
+    text-align: center;
+    font-weight: bold;
+    cursor: pointer;
+  }
+
+  .grey {
+    background-color: #ccc;
+    color: #333;
+
+    &:hover {
+      background-color: #ddd;
+    }
+  }
+
+  .dark {
+    background-color: #444;
+    color: #fff;
+
+    &:hover {
+      background-color: #555;
+    }
+  }
+
+  .orange {
+    background-color: #e08d1f;
+    color: #fff;
+
+    &:hover {
+      background-color: #fda22b;
+    }
+  }
+}
+</style>
