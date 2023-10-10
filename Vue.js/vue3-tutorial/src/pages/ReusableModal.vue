e13e069fca11904c26f7fe5e0e652c75df982bf3
Jonny Soth <janysoth@gmail.com>
2023-10-10 05:01:09 -0500

    Create Modal.vue as  child component.

--- Vue.js/vue3-tutorial/src/pages/ReusableModal.vue
+++ Vue.js/vue3-tutorial/src/pages/ReusableModal.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>Reusable Modal</div>
+  <div>
+    <section class="flex w-full">
+      <div class="m-auto">
+        <h1 class="text-2xl my-5">Re-Usable Modal</h1>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
