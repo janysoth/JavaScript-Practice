6dbc0a9eaba52dbf190bc0c42741cc05cdce05d1
Jonny Soth <janysoth@gmail.com>
2023-10-10 05:28:01 -0500

    create close function to close the modal.

--- Vue.js/vue3-tutorial/src/components/LoginModal.vue
+++ Vue.js/vue3-tutorial/src/components/LoginModal.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <section @click="close" class="z-20 h-screen w-screen bg-gray-500 fixed top-0 opacity-50"></section>
+        <section @click="close" class="z-20 h-screen w-screen bg-gray-500 fixed top-0 opacity-50" />
         <div class="absolute inset-0">
             <div class="flex h-full">
                 <div class="z-30 m-auto bg-white rounded p-2 shadow w-1/3">
--- Vue.js/vue3-tutorial/src/pages/ReusableModal.vue
+++ Vue.js/vue3-tutorial/src/pages/ReusableModal.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
-    <section class="flex w-full">
-      <div class="m-auto">
-        <h1 class="text-2xl my-5">Re-Usable Modal</h1>
-      </div>
-    </section>
-  </div>
+  <section class="flex flex-wrap w-full">
+    <div class="m-auto">
+      <h1 class="text-2xl my-5">Re-Usable Modal</h1>
+      <Modal v-if="isModalOpen" @close="isModalOpen = false" />
+    </div>
+  </section>
 </template>
 
 <script>
+import Modal from "@/components/ModalComp.vue";
+import { ref } from 'vue';
+
 export default {
+  components: { Modal },
+
+  setup() {
+    const isModalOpen = ref(true);
+
+    return { isModalOpen };
+  }
 
 }
 </script>
