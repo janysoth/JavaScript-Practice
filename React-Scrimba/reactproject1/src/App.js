478d11075d111e9c6a7dd9201040fa2aab3f0370
Jonny Soth <janysoth@gmail.com>
2024-01-25 05:26:22 -0600

    Create react file using npx create-react-app.

---
+++ React-Scrimba/reactproject1/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Website</h1>
+      <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
