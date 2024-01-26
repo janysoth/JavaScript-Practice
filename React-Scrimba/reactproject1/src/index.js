a501bf2df828046972dc2e33cda48aaa799f8635
janysoth <janysoth@gmail.com>
2024-01-26 10:07:59 -0600

    Learn about React18 by using createRoot function.

--- React-Scrimba/reactproject1/src/index.js
+++ React-Scrimba/reactproject1/src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const navbar = (
+  <nav>
+    <h1>Bob's Bistro</h1>
+    <ul>
+      <li>Menu</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
 );
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(navbar);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(navbar);
