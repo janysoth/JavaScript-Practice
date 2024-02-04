42e6d40e929ee53fd2f8ddfdfeddf6dd466bd1ab
Jonny Soth <janysoth@gmail.com>
2024-02-04 05:41:54 -0600

    Create components folder to store NavBar and MainContent files.

---
+++ React-Scrimba/vite-react/src/components/MainContent.jsx
@@ -0,0 +1,7 @@
+function MainContent () {
+  return (
+    <h1>Main Content goes here</h1>
+  )
+}
+
+export default MainContent
---
+++ React-Scrimba/vite-react/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import reactLogo from "../assets/react.svg";
+
+function NavBar () {
+  return (
+    <div>
+      <img src={reactLogo} className="logo"/>
+      <h1>NavBar goes here</h1>
+    </div>
+  )
+}
+
+export default NavBar
