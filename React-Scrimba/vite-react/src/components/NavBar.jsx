2dae8415e2c2328c66e0ca5f8c6c72c0e7089f25
Jonny Soth <janysoth@gmail.com>
2024-02-06 05:21:29 -0600

    Change className for all of the div.

--- React-Scrimba/vite-react/src/components/NavBar.jsx
+++ React-Scrimba/vite-react/src/components/NavBar.jsx
@@ -4,9 +4,9 @@ import reactLogo from "../assets/react.svg";
 function NavBar () {
   return (
     <nav>
-      <img src={reactLogo} className="logo"/>
-      <h3>ReacFacts</h3>
-      <h4>React Course - Project 1</h4>
+      <img src={reactLogo} className="nav--icon"/>
+      <h3 class="nav--logo_text">ReactFacts</h3>
+      <h4 class="nav--title">React Course - Project 1</h4>
     </nav>
   )
 }
