9c849be72b44df53df4f2d195194fe7b5a5f1001
Jonny Soth <janysoth@gmail.com>
2024-02-08 05:51:02 -0600

    Create airBnbClone-react project.

---
+++ React-Scrimba/airBnbClone-react/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import reactLogo from "../assets/airbnb-logo.png";
+
+function NavBar () {
+return (
+  <div>
+    <img src={reactLogo} />
+  </div>
+)
+}
+
+export default NavBar
