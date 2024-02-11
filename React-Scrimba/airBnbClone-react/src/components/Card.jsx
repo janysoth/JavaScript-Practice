a5c57a984cc4dbab0a73a07131ea0f759cdf2f50
janysoth <janysoth@gmail.com>
2024-02-11 07:10:35 -0600

    Add alt tag in the img.

--- React-Scrimba/airBnbClone-react/src/components/Card.jsx
+++ React-Scrimba/airBnbClone-react/src/components/Card.jsx
@@ -4,14 +4,22 @@ import star from "../assets/star.png"
 function Card () {
   return (
     <div className="card">
-        <img src={katieZPhoto} className="card--image" />
+        <img 
+            src={katieZPhoto} 
+            className="card--image"
+            alt="Main Card Image" 
+        />
         <div className="card--stats">
-          <img src={star} className="card--star"/>
+          <img 
+              src={star} 
+              className="card--star"
+              alt="Star Icon"
+          />
           <span>5.0</span>
           <span className="gray">(6) • </span>
           <span className="gray">USA</span>
         </div>
-        <p>Life Lessons with Katie Zaferes</p>
+        <h2>Life Lessons with Katie Zaferes</h2>
         <p><span className="bold">From $136</span> / person</p>
     </div>
   )
