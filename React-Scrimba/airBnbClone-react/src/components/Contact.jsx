cdbe0a864eaa2d1fdb064445f1c6c9860261dad2
Jonny Soth <janysoth@gmail.com>
2024-02-13 23:47:55 -0600

    Learn about destruction of object.

--- React-Scrimba/airBnbClone-react/src/components/Contact.jsx
+++ React-Scrimba/airBnbClone-react/src/components/Contact.jsx
@@ -1,5 +1,18 @@
-function Contact () {
-
+function Contact ({img, name, phone, email}) {
+    return (
+        <div className="contact-card">
+            <img src={img}/>
+            <h3>{name}</h3>
+            <div className="info-group">
+                <img src="./images/phone-icon.png" />
+                <p>{phone}</p>
+            </div>
+            <div className="info-group">
+                <img src="./images/mail-icon.png" />
+                <p>{email}</p>
+            </div>
+        </div>
+    )
 }
 
 export default Contact
