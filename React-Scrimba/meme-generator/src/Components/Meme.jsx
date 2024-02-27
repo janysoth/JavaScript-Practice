53ae15672f9cfc42617da7d26a21f89b1d0a4b14
Jonny Soth <janysoth@gmail.com>
2024-02-27 05:42:20 -0600

    Add a label for each input.

--- React-Scrimba/meme-generator/src/Components/Meme.jsx
+++ React-Scrimba/meme-generator/src/Components/Meme.jsx
@@ -2,14 +2,18 @@ function Meme(){
   return (
         <main>
             <form className="form">
-                <input 
-                  type="text"
-                  placeholder="Top Text" 
-                  className="form--input" />
-                <input 
-                  type="text"
-                  placeholder="Bottom Text" 
-                  className="form--input" />
+                <label>Top Text
+                  <input
+                    type="text"
+                    placeholder="Shut up"
+                    className="form--input" />
+                </label>
+                <label>Bottom Text
+                  <input
+                    type="text"
+                    placeholder="and take my money"
+                    className="form--input" />
+                </label>
                 <button className="form--button">Get a new meme image 🖼</button>
             </form>
         </main>
