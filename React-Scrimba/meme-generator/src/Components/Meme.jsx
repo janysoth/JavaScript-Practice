84a216be8714b6af43fb3b1089cd1467905c32f5
Jonny Soth <janysoth@gmail.com>
2024-02-28 05:39:52 -0600

    Style the input to sit side to side.

--- React-Scrimba/meme-generator/src/Components/Meme.jsx
+++ React-Scrimba/meme-generator/src/Components/Meme.jsx
@@ -1,23 +1,27 @@
-function Meme(){
+function Meme() {
   return (
-        <main>
-            <form className="form">
-                <label htmlFor="top-text">Top Text</label>
-                  <input
-                    id="top-text"
-                    type="text"
-                    placeholder="Shut up"
-                    className="form--input" />
-                <label htmlFor="bottom-text">Bottom Text</label>
-                  <input
-                    id="bottom-text"
-                    type="text"
-                    placeholder="and take my money"
-                    className="form--input" />
-                <button className="form--button">Get a new meme image 🖼</button>
-            </form>
-        </main>
-  )
+    <main>
+      <form className="form">
+        <div className="form--input-container">
+          <label htmlFor="top-text">Top Text</label>
+          <input
+            id="top-text"
+            type="text"
+            placeholder="Shut up"
+            className="form--input" />
+        </div>
+        <div className="form--input-container">
+          <label htmlFor="bottom-text">Bottom Text</label>
+          <input
+            id="bottom-text"
+            type="text"
+            placeholder="and take my money"
+            className="form--input" />
+        </div>
+        <button className="form--button">Get a new meme image 🖼</button>
+      </form>
+    </main>
+  );
 }
 
-export default Meme
+export default Meme;
