a46e4d80fb4ca72051ef2b1ecb7c8c3d415d7ac2
Jonny Soth <janysoth@gmail.com>
2024-02-28 05:26:24 -0600

    Learn a different way to apply label for the input field.

--- React-Scrimba/meme-generator/src/Components/Meme.jsx
+++ React-Scrimba/meme-generator/src/Components/Meme.jsx
@@ -2,18 +2,18 @@ function Meme(){
   return (
         <main>
             <form className="form">
-                <label>Top Text
+                <label htmlFor="top-text">Top Text</label>
                   <input
+                    id="top-text"
                     type="text"
                     placeholder="Shut up"
                     className="form--input" />
-                </label>
-                <label>Bottom Text
+                <label htmlFor="bottom-text">Bottom Text</label>
                   <input
+                    id="bottom-text"
                     type="text"
                     placeholder="and take my money"
                     className="form--input" />
-                </label>
                 <button className="form--button">Get a new meme image 🖼</button>
             </form>
         </main>
