a9a622c8f4dabf40de1127d45037e54a9212c172
Jonny Soth <janysoth@gmail.com>
2024-02-29 05:21:10 -0600

    Create memesData.js to generate the memes.

--- React-Scrimba/meme-generator/src/Components/Meme.jsx
+++ React-Scrimba/meme-generator/src/Components/Meme.jsx
@@ -1,25 +1,23 @@
 function Meme() {
   return (
     <main>
-      <form className="form">
-        <div className="form--input-container">
-          <label htmlFor="top-text">Top Text</label>
-          <input
-            id="top-text"
+      <div className="form">
+        <input 
             type="text"
-            placeholder="Shut up"
-            className="form--input" />
-        </div>
-        <div className="form--input-container">
-          <label htmlFor="bottom-text">Bottom Text</label>
-          <input
-            id="bottom-text"
+            placeholder="Top text"
+            className="form--input"
+        />
+        <input 
             type="text"
-            placeholder="and take my money"
-            className="form--input" />
-        </div>
-        <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+            placeholder="Bottom text"
+            className="form--input"
+        />
+        <button 
+            className="form--button"
+        >
+            Get a new meme image 🖼
+        </button>
+      </div>
     </main>
   );
 }
