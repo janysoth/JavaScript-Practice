8f8ea52c52a0633e1a8bda961ea3ccddfcd3000b
Jonny Soth <janysoth@gmail.com>
2024-03-02 05:06:07 -0600

    Learn how to use state function to update value in React.

---
+++ React-Scrimba/meme-generator/src/Components/Practice.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+function Practice () {
+    let [count, setCount] = React.useState(0)
+    
+    function add (){
+      setCount(count + 1)
+    }
+
+    function minus(){
+      if (count > 0)
+      setCount(count - 1)
+      else
+      setCount(count = 0)
+    }
+    return (
+        <div className="counter">
+            <button className="counter--minus" onClick={minus}>–</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
+            </div>
+            <button className="counter--plus" onClick={add}>+</button>
+        </div>
+    )
+}
+
+export default Practice;
