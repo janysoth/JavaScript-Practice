d85668297499e3bc5319d98345969ce1fe904795
janysoth <janysoth@gmail.com>
2024-03-05 07:43:31 -0600

    Learn how to useState with ternary to change the value of the div.

--- React-Scrimba/meme-generator/src/Components/Practice.jsx
+++ React-Scrimba/meme-generator/src/Components/Practice.jsx
@@ -1,4 +1,4 @@
-// import React from "react"
+import React from "react"
 
 // function Practice () {
 //     let [count, setCount] = React.useState(0)
@@ -25,15 +25,18 @@
 // }
 
 function Practice () {
-      const isGoingOut = true
-    
-    let answer = isGoingOut ===true ? "Yes" : "No"
+
+    const [isGoingOut, setIsGoingOut] = React.useState(true);
+
+    function changeMind(){
+      setIsGoingOut(prevState => !prevState)
+    }
 
     return (
         <div className="state">
             <h1 className="state--title">Do I feel like going out tonight?</h1>
-            <div className="state--value">
-                <h1>{answer}</h1>
+            <div onClick={changeMind} className="state--value">
+                <h1>{isGoingOut ? "Yes" : "No"}</h1>
             </div>
         </div>
     )
