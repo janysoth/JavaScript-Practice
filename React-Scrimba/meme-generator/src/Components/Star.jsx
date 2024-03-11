e3720840f3f4ed598d068aff576a616d674759b9
janysoth <janysoth@gmail.com>
2024-03-11 00:10:03 -0500

    Learn how to fix the accessibility issues.

--- React-Scrimba/meme-generator/src/Components/Star.jsx
+++ React-Scrimba/meme-generator/src/Components/Star.jsx
@@ -1,14 +1,22 @@
 /* eslint-disable react/prop-types */
 function Star (props) {
   const starIcon = props.isFilled ? "star-filled.png" : "star-empty.png"
+  const buttonLabel = props.isFilled ? "Unmark as favorite" :  "Mark as favorite"
 
   return (
     <div>
-      <img 
-        src={`../images/${starIcon}`} 
-        className="card--favorite"
+      <button 
         onClick={props.handleClick}
-      />
+        aria-label={buttonLabel}
+        aria-pressed= {props.isFilled}
+        className="card--favorite--button"
+      >
+        <img
+          src={`../images/${starIcon}`}
+          alt="star icon"
+          className="card--favorite"
+        />
+      </button>
     </div>
   )
 }
