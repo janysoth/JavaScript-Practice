0384edb03109a92a851cf2202b3f5aa41280e87f
Jonny Soth <janysoth@gmail.com>
2024-03-12 05:42:39 -0500

    Learn how to pass data around in React.

--- React-Scrimba/meme-generator/src/Components/Header.jsx
+++ React-Scrimba/meme-generator/src/Components/Header.jsx
@@ -1,11 +1,28 @@
-function Header(){
-  return (
-    <header className="header">
-      <img className="header--image" src="./images/troll-face.png"/>
-      <h2 className="header--title">Meme Generator</h2>
-      <h4 className="header--project">React Course - Project 3</h4>
-    </header>
-  )
+// function Header(){
+//   return (
+//     <header className="header">
+//       <img className="header--image" src="./images/troll-face.png"/>
+//       <h2 className="header--title">Meme Generator</h2>
+//       <h4 className="header--project">React Course - Project 3</h4>
+//     </header>
+//   )
+// }
+
+import React from "react"
+function Header(props) {
+    
+    /**
+     * Challenge:
+     * Raise state up a level and pass it down to both the
+     * Header and Body components through props.
+     */
+    // const [user, setUser] = React.useState("Joe")
+    
+    return (
+        <header>
+            <p>Current user: {props.user}</p>
+        </header>
+    )
 }
 
 export default Header
--- React-Scrimba/meme-generator/src/Components/Practice.jsx
+++ React-Scrimba/meme-generator/src/Components/Practice.jsx
@@ -1,38 +1,51 @@
 import React from "react"
-import Star from "./Star";
+// import Star from "./Star";
+import Header from "./Header"
+import Body from "./Body"
+
+// function Practice () {
+//     const [contact, setContact] = React.useState({
+//         firstName: "John",
+//         lastName: "Doe",
+//         phone: "+1 (719) 555-1212",
+//         email: "itsmyrealname@example.com",
+//         isFavorite: true
+//     })
+
+//     function toggleFavorite() {
+//         setContact(prevContact => {
+//             return {
+//                 ...prevContact, 
+//                 isFavorite: !prevContact.isFavorite
+//             }
+//         })
+//     }
+    
+//     return (
+//         <main>
+//             <article className="card">
+//                 <img src="./images/user.png" className="card--image" />
+//                 <div className="card--info">
+//                 <Star isFilled={contact.isFavorite} handleClick={toggleFavorite}/>
+//                     <h2 className="card--name">
+//                         {contact.firstName} {contact.lastName}
+//                     </h2>
+//                     <p className="card--contact">{contact.phone}</p>
+//                     <p className="card--contact">{contact.email}</p>
+//                 </div>
+                
+//             </article>
+//         </main>
+//     )
+// }
 
 function Practice () {
-    const [contact, setContact] = React.useState({
-        firstName: "John",
-        lastName: "Doe",
-        phone: "+1 (719) 555-1212",
-        email: "itsmyrealname@example.com",
-        isFavorite: true
-    })
 
-    function toggleFavorite() {
-        setContact(prevContact => {
-            return {
-                ...prevContact, 
-                isFavorite: !prevContact.isFavorite
-            }
-        })
-    }
-    
+    const [user, setUser] = React.useState("Jonny")
     return (
         <main>
-            <article className="card">
-                <img src="./images/user.png" className="card--image" />
-                <div className="card--info">
-                <Star isFilled={contact.isFavorite} handleClick={toggleFavorite}/>
-                    <h2 className="card--name">
-                        {contact.firstName} {contact.lastName}
-                    </h2>
-                    <p className="card--contact">{contact.phone}</p>
-                    <p className="card--contact">{contact.email}</p>
-                </div>
-                
-            </article>
+            <Header user = {user} />
+            <Body user = {user} />
         </main>
     )
 }
