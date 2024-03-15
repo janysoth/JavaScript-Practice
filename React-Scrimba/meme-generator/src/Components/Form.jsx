e8550e992fea2c8364c431c8045dc7b52ccde661
Jonny Soth <janysoth@gmail.com>
2024-03-15 05:41:24 -0500

    Learn about state input change in React.

---
+++ React-Scrimba/meme-generator/src/Components/Form.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function Form() {
+    const [firstName, setFirstName] = React.useState("")
+    
+    console.log(firstName)
+
+    const handleChange = (event) => {
+      setFirstName(event.target.value)
+    }
+    return (
+        <form>
+            <input
+                type="text"
+                placeholder="First Name"
+                onChange={handleChange}
+            />
+        </form>
+    )
+}
