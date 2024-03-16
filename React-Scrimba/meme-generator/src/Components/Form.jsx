42f5ddf534b06a5498c3dc336f9228d724610c60
Jonny Soth <janysoth@gmail.com>
2024-03-16 03:06:03 -0500

    Learn about controlled input.

--- React-Scrimba/meme-generator/src/Components/Form.jsx
+++ React-Scrimba/meme-generator/src/Components/Form.jsx
@@ -18,25 +18,33 @@ export default function Form() {
       })
     }
     return (
-        <form>
-            <input
-                type="text"
-                placeholder="First Name"
-                onChange={handleChange}
-                name="firstName"
-            />
-            <input
-                type="text"
-                placeholder="Last Name"
-                onChange={handleChange}
-                name="lastName"
-            />
-            <input
-                type="email"
-                placeholder="Email"
-                onChange={handleChange}
-                name="email"
-            />
-        </form>
+        <div>
+          <form>
+              <input
+                  type="text"
+                  placeholder="First Name"
+                  onChange={handleChange}
+                  name="firstName"
+                  value={formData.firstName}
+              />
+              <input
+                  type="text"
+                  placeholder="Last Name"
+                  onChange={handleChange}
+                  name="lastName"
+                  value={formData.lastName}
+              />
+              <input
+                  type="email"
+                  placeholder="Email"
+                  onChange={handleChange}
+                  name="email"
+                  value={formData.email}
+              />
+          </form>
+          <h2>First Name: {formData.firstName}</h2>
+          <h2>Last Name: {formData.lastName}</h2>
+          <h2>Email: {formData.email}</h2>
+        </div>
     )
 }
