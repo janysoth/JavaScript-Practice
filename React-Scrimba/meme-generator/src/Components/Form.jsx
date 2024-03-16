07466dcb11ac12cd7ab077cac3e32c9ed882d422
Jonny Soth <janysoth@gmail.com>
2024-03-16 03:35:39 -0500

    Learn about checkbox in React.

--- React-Scrimba/meme-generator/src/Components/Form.jsx
+++ React-Scrimba/meme-generator/src/Components/Form.jsx
@@ -6,15 +6,17 @@ export default function Form() {
       lastName: "",
       email: "",
       comments: "",
+      isFriendly: true,
     })
     
     console.log(formData)
 
     const handleChange = (event) => {
+      const {name, value, type, checked} = event.target
       setFormData(prevFormData => {
         return {
           ...prevFormData,  
-          [event.target.name]: event.target.value
+          [name]: type === "checkbox" ? checked : value
         }
       })
     }
@@ -22,36 +24,46 @@ export default function Form() {
         <div>
           <form>
               <input
-                  type="text"
-                  placeholder="First Name"
-                  onChange={handleChange}
-                  name="firstName"
-                  value={formData.firstName}
+                type="text"
+                placeholder="First Name"
+                onChange={handleChange}
+                name="firstName"
+                value={formData.firstName}
               />
               <input
-                  type="text"
-                  placeholder="Last Name"
-                  onChange={handleChange}
-                  name="lastName"
-                  value={formData.lastName}
+                type="text"
+                placeholder="Last Name"
+                onChange={handleChange}
+                name="lastName"
+                value={formData.lastName}
               />
               <input
-                  type="email"
-                  placeholder="Email"
-                  onChange={handleChange}
-                  name="email"
-                  value={formData.email}
+                type="email"
+                placeholder="Email"
+                onChange={handleChange}
+                name="email"
+                value={formData.email}
               />
               <textarea 
-                  placeholder="Please leave your comment here..."
-                  onChange={handleChange}
-                  name="comments"
-                  value={formData.comments}
+                placeholder="Please leave your comment here..."
+                onChange={handleChange}
+                name="comments"
+                value={formData.comments}
               />
+            <input 
+                type="checkbox" 
+                id="isFriendly" 
+                checked={formData.isFriendly}
+                onChange={handleChange}
+                name="isFriendly"
+            />
+            <label htmlFor="isFriendly">Are you friendly?</label>
+            <br />
           </form>
           <h2>First Name: {formData.firstName}</h2>
           <h2>Last Name: {formData.lastName}</h2>
           <h2>Email: {formData.email}</h2>
+          <h2>Is Friendly: {formData.isFriendly.value}</h2>
         </div>
     )
 }
