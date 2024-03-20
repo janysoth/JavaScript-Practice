64d588c193fe1ba0ea4911c1a863a2145ef1ae8d
Jonny Soth <janysoth@gmail.com>
2024-03-20 05:37:18 -0500

    Add text to image.

--- React-Scrimba/meme-generator/src/App.jsx
+++ React-Scrimba/meme-generator/src/App.jsx
@@ -1,15 +1,15 @@
-// import Header from "./Components/Header"
-// import Meme from "./Components/Meme"
-import Form from "./Components/Form"
+import Header from "./Components/Header"
+import Meme from "./Components/Meme"
+// import Form from "./Components/Form"
 // import Practice from "./Components/Practice"
 function App() {
 
   return (
     <div>
-      {/* <Header />
-      <Meme />  */}
+      <Header />
+      <Meme />
       {/* <Practice /> */}
-      <Form />
+      {/* <Form /> */}
     </div>
   )
 }
