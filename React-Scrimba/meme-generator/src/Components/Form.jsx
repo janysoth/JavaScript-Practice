0e67177e2ed0f875feb569d13a54e5a1e446fd96
Jonny Soth <janysoth@gmail.com>
2024-03-20 00:20:00 -0500

    Create handleSubmit function to submit the form.

--- React-Scrimba/meme-generator/src/Components/Form.jsx
+++ React-Scrimba/meme-generator/src/Components/Form.jsx
@@ -1,27 +1,6 @@
 import React from "react"
 
 export default function App() {
-
-  /**
-   * Challenge: Connect the form to local state
-   * 
-   * 1. Create a state object to store the 4 values we need to save.
-   * 2. Create a single handleChange function that can
-   *    manage the state of all the inputs and set it up
-   *    correctly
-   * 3. When the user clicks "Sign up", check if the 
-   *    password & confirmation match each other. If
-   *    so, log "Successfully signed up" to the console.
-   *    If not, log "passwords to not match" to the console.
-   * 4. Also when submitting the form, if the person checked
-   *    the "newsletter" checkbox, log "Thanks for signing
-   *    up for our newsletter!" to the console.
-   */
-
-  function handleSubmit(event) {
-    event.preventDefault()
-  }
-
   const [formData, setFormData] = React.useState({
     email: "",
     password: "",
@@ -35,6 +14,19 @@ export default function App() {
       ...prevFormData,
       [name]: type === "checkbox" ? checked : value
     }))
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    if (formData.password === formData.passwordConfirm)
+      console.log("Successfully sign up. Thank you.")
+    else {
+      console.log("Password do not match. Please try again.")
+      return
+    }
+
+    formData.joinedNewsletter && console.log("Thanks for signing up for our newsletter!")
   }
 
   console.log(formData)
