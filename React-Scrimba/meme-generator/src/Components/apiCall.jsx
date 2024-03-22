30b501a046b45a4b1b1cb0a2ad1d91e48a3b9120
Jonny Soth <janysoth@gmail.com>
2024-03-22 05:26:21 -0500

    Learn about useEffect in React.

--- React-Scrimba/meme-generator/src/Components/apiCall.jsx
+++ React-Scrimba/meme-generator/src/Components/apiCall.jsx
@@ -3,12 +3,11 @@ import React from "react"
 export default function ApiCall() {
   const [starWarsData, setStarWarsData] = React.useState({})
 
-  // 1. GET the data (fetch)
-  // 2. Save the data to state
-
-  fetch("https://swapi.dev/api/people/1")
-    .then(res => res.json())
-    .then(data => setStarWarsData(data))
+  React.useEffect(() => {
+    fetch("https://swapi.dev/api/people/1")
+      .then(res => res.json())
+      .then(data => setStarWarsData(data))
+  })
 
   return (
     <div>
