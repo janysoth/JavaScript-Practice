7f575154392cfb8f2ea8fd116c2f4fe1c11e2056
janysoth <janysoth@gmail.com>
2024-03-25 03:10:01 -0500

    Learn about useEffect cleanup.

--- React-Scrimba/meme-generator/src/Components/WindowTracker.jsx
+++ React-Scrimba/meme-generator/src/Components/WindowTracker.jsx
@@ -5,9 +5,20 @@ export default function WindowTracker() {
   const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
 
   React.useEffect(() => {
-    window.addEventListener("resize", function() {
+    const watchWidth = () => {
+      console.log("Window Resized")
       setWindowWidth(window.innerWidth)
-    })
+    }
+
+    window.addEventListener("resize", watchWidth)
+
+    // useEffect Cleanup 
+    // To clean up any sideEffect
+    // use function to  ensure the event listener is removed when this component unmounts
+    return function () {
+      console.log("Cleaning up")
+      window.removeEventListener("resize", watchWidth)
+    }
   }, [])
 
   return (
