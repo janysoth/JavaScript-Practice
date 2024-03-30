5acaed8fdc05086af6b72ea6d0501e5e85a8c848
janysoth <janysoth@gmail.com>
2024-03-30 07:09:21 -0500

    Create a function to delete noters.

--- React-Scrimba/notes-app/src/App.jsx
+++ React-Scrimba/notes-app/src/App.jsx
@@ -39,14 +39,49 @@ export default function App() {
     setCurrentNoteId(newNote.id)
   }
 
+  /** Long Version of updateNote Function
   function updateNote(text) {
-    setNotes(oldNotes => oldNotes.map(oldNote => {
-      return oldNote.id === currentNoteId
-        ? { ...oldNote, body: text }
-        : oldNote
-    }))
+    // To put the most recently-modified notes at the top
+    setNotes(oldNotes => {
+      // Create a new empty array
+      const newNoteArray = []
+      // Loop over the original array 
+      for (let i = 0; i < oldNotes.length; i++) {
+        const oldNote = oldNotes[i]
+        if(oldNote.id === currentNoteId) {
+          newNoteArray.unshift({
+            ...oldNote, body: text
+          })
+        } else {
+          newNoteArray.push(oldNote)
+        }
+      }
+      return newNoteArray
+    })
+  }
+  -----------------*/
+
+  // Short Version of updateNote Function
+  function updateNote(text) {
+    setNotes(oldNotes => {
+      const updateNotes = oldNotes.map(oldNote => {
+        if(oldNote.id === currentNoteId) {
+          return {...oldNote, body: text}
+        }
+        return oldNote
+      })
+      
+      // Use the find function to get the recetly updated note
+      // Then use the filter function to put the rest of notes back in order
+      return [updateNotes.find(note => note.id === currentNoteId), 
+        ...updateNotes.filter(note => note.id !== currentNoteId)]
+    })
   }
 
+  function deleteNote(event, noteId){
+    event.stopPropagation()
+    setNotes(oldNotes => oldNotes.filter(note => note.id !== noteId))
+  }
   function findCurrentNote() {
     return notes.find(note => {
       return note.id === currentNoteId
@@ -68,6 +103,7 @@ export default function App() {
               currentNote={findCurrentNote()}
               setCurrentNoteId={setCurrentNoteId}
               newNote={createNewNote}
+              deleteNote={deleteNote}
             />
             {
               currentNoteId &&
