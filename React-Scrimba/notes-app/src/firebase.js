3cdb62535c3e901130ef4e97606637c0ba7a928e
Jonny Soth <janysoth@gmail.com>
2024-04-03 06:03:58 -0500

    Update firebaseConfig.

--- React-Scrimba/notes-app/src/firebase.js
+++ React-Scrimba/notes-app/src/firebase.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-unused-vars */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection } from "firebase/firestore";
+import { getFirestore, collection } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2umtVTeaNDjkx7kFelYDaD6SBIIT9KiQ",
-  authDomain: "react-notes-1e6dd.firebaseapp.com",
-  projectId: "react-notes-1e6dd",
-  storageBucket: "react-notes-1e6dd.appspot.com",
-  messagingSenderId: "840146123607",
-  appId: "1:840146123607:web:21b35f3fb118bd935e1c12"
+  apiKey: "AIzaSyADAHyUOJ3fMFomCgiacD3HAEge7pLnYQ0",
+  authDomain: "react-notes-8ffcf.firebaseapp.com",
+  projectId: "react-notes-8ffcf",
+  storageBucket: "react-notes-8ffcf.appspot.com",
+  messagingSenderId: "1030863313425",
+  appId: "1:1030863313425:web:cbbc2c0481430feb866fb0"
 };
 
 // Initialize Firebase
