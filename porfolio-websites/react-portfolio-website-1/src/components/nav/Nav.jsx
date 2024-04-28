cede80621c47248627ba948aee0f181480a216d4
janysoth <janysoth@gmail.com>
2024-04-27 21:09:43 -0500

    Create a tag for the nav bar.

--- porfolio-websites/react-portfolio-website-1/src/components/nav/Nav.jsx
+++ porfolio-websites/react-portfolio-website-1/src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import { IoHomeOutline } from "react-icons/io5";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { RiServiceFill } from "react-icons/ri";
+import { BiMessageDetail } from "react-icons/bi";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#'><IoHomeOutline /></a>
+      <a href='#about'><AiOutlineUser /></a>
+      <a href='#experience'><BiBook /></a>
+      <a href='#services'><RiServiceFill /></a>
+      <a href='#contact'><BiMessageDetail /></a>
+    </nav>
   )
 }
 
