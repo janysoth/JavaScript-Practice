10fd2c1bf5f224d2f57deaca6fdbf57a38db621a
janysoth <janysoth@gmail.com>
2024-04-29 00:01:08 -0500

    Create about component.

--- porfolio-websites/react-portfolio-website-1/src/components/about/About.jsx
+++ porfolio-websites/react-portfolio-website-1/src/components/about/About.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/KhmerFamilyPic.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Me" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>300+ Clients Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>80+ Projects</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit aliquid impedit nam quas, pariatur ipsa quo eaque et libero minima hic, ipsam repellat veniam nulla deserunt vel sunt fugiat vitae.
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
