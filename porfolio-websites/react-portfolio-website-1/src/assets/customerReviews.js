57f498a9911288587e63fa0c85ebec77be90ac53
Jonny Soth <janysoth@gmail.com>
2024-05-04 02:00:58 -0500

    Create markup for  Testimonials component.

---
+++ porfolio-websites/react-portfolio-website-1/src/assets/customerReviews.js
@@ -0,0 +1,27 @@
+import AVTR1 from "./avatar1.jpeg"
+import AVTR2 from "./avatar2.jpeg"
+import AVTR3 from "./avatar3.jpeg"
+import AVTR4 from "./avatar4.jpeg"
+
+export default [
+  {
+    avatar: AVTR1,
+    name: "Tina Snow",
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem illum eius, dolorem perspiciatis minima labore minus sunt velit repudiandae voluptas quae omnis placeat facilis quos non nisi dolor est beatae."
+  },
+  {
+    avatar: AVTR2,
+    name: "Andy Lee",
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem illum eius, dolorem perspiciatis minima labore minus sunt velit repudiandae voluptas quae omnis placeat facilis quos non nisi dolor est beatae."
+  },
+  {
+    avatar: AVTR3,
+    name: "Shatta Wale",
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem illum eius, dolorem perspiciatis minima labore minus sunt velit repudiandae voluptas quae omnis placeat facilis quos non nisi dolor est beatae."
+  },
+  {
+    avatar: AVTR4,
+    name: "John Jeffersons",
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem illum eius, dolorem perspiciatis minima labore minus sunt velit repudiandae voluptas quae omnis placeat facilis quos non nisi dolor est beatae."
+  },
+];
--- porfolio-websites/react-portfolio-website-1/src/components/testimonials/Testimonials.jsx
+++ porfolio-websites/react-portfolio-website-1/src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './testimonials.css'
+import customerReviews from '../../assets/customerReviews'
 
 const Testimonials = () => {
   return (
-    <section id="testimonials">Testimonials</section>
+    <section id="testimonials">
+      <h5>Reviews from Clients</h5>
+      <h2>Testimonials</h2>
+
+      {
+        customerReviews.map((data) => {
+          return (
+            <div className="container testimonials__container">
+              <article className="testimonial">
+                <div className="client__avatar">
+                  <img src={data.avatar} alt="Avatar One" />
+                </div>
+                <h5>{data.name}</h5>
+                <small className="client__review">
+                  {data.review}
+                </small>
+              </article>
+            </div>
+          )
+        })}
+    </section>
   )
 }
 
