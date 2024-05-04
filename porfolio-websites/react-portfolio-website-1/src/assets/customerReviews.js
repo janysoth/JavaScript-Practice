949f05ec89e55b32b6d6c1631b46d579a04f095c
Jonny Soth <janysoth@gmail.com>
2024-05-04 02:53:04 -0500

    Finish styling Testimonials component and learn how to use swiperjs.

--- porfolio-websites/react-portfolio-website-1/src/assets/customerReviews.js
+++ porfolio-websites/react-portfolio-website-1/src/assets/customerReviews.js
@@ -5,21 +5,25 @@ import AVTR4 from "./avatar4.jpeg"
 
 export default [
   {
+    id: 1,
     avatar: AVTR1,
     name: "Tina Snow",
     review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem illum eius, dolorem perspiciatis minima labore minus sunt velit repudiandae voluptas quae omnis placeat facilis quos non nisi dolor est beatae."
   },
   {
+    id: 2,
     avatar: AVTR2,
     name: "Andy Lee",
     review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem illum eius, dolorem perspiciatis minima labore minus sunt velit repudiandae voluptas quae omnis placeat facilis quos non nisi dolor est beatae."
   },
   {
+    id: 3,
     avatar: AVTR3,
     name: "Shatta Wale",
     review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem illum eius, dolorem perspiciatis minima labore minus sunt velit repudiandae voluptas quae omnis placeat facilis quos non nisi dolor est beatae."
   },
   {
+    id: 4,
     avatar: AVTR4,
     name: "John Jeffersons",
     review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem illum eius, dolorem perspiciatis minima labore minus sunt velit repudiandae voluptas quae omnis placeat facilis quos non nisi dolor est beatae."
--- porfolio-websites/react-portfolio-website-1/src/components/testimonials/Testimonials.jsx
+++ porfolio-websites/react-portfolio-website-1/src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
-import './testimonials.css'
-import customerReviews from '../../assets/customerReviews'
+import React from 'react';
+import './testimonials.css';
+import customerReviews from '../../assets/customerReviews';
+
+// Import Swiper core and required modules
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 
 const Testimonials = () => {
   return (
@@ -8,11 +17,16 @@ const Testimonials = () => {
       <h5>Reviews from Clients</h5>
       <h2>Testimonials</h2>
 
-      {
-        customerReviews.map((data) => {
-          return (
-            <div className="container testimonials__container">
-              <article className="testimonial">
+      <Swiper className="container testimonials__container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {
+          customerReviews.map((data) => {
+            return (
+              <SwiperSlide key={data.id} className="testimonial">
                 <div className="client__avatar">
                   <img src={data.avatar} alt="Avatar One" />
                 </div>
@@ -20,12 +34,13 @@ const Testimonials = () => {
                 <small className="client__review">
                   {data.review}
                 </small>
-              </article>
-            </div>
-          )
-        })}
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
     </section>
   )
 }
 
-export default Testimonials
+export default Testimonials;
