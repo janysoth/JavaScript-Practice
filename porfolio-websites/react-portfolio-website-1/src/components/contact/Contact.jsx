94d169f665f23ff4cc220a21849befce316d91ba
janysoth <janysoth@gmail.com>
2024-05-05 09:49:25 -0500

    Finish the markup for the contact component.

--- porfolio-websites/react-portfolio-website-1/src/components/contact/Contact.jsx
+++ porfolio-websites/react-portfolio-website-1/src/components/contact/Contact.jsx
@@ -1,9 +1,47 @@
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contat Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>janysoth@gmail.com</h5>
+            <a href="mailto:janysoth@gmail.com" target='_blank'>Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine/>
+            <h4>Messenger</h4>
+            <h5>Jonny Vorn Andre Soth</h5>
+            <a href="https://m.me/JVAndreSOTH" target='_blank'>Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp/>
+            <h4>Whatsapp</h4>
+            <h5>+123456789</h5>
+            <a href="https://api.whatsapp.com/send?phone=+15072715870" target='_blank'>Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="text" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
