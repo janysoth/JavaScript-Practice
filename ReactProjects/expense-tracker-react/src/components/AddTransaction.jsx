9d6b0aaf4a7c54ec9faee4eecbd69a8ca4b51275
Jonny Soth <janysoth@gmail.com>
2024-05-10 05:33:03 -0500

    Create useState for descriptionText and amount for AddTransaction component.

--- ReactProjects/expense-tracker-react/src/components/AddTransaction.jsx
+++ ReactProjects/expense-tracker-react/src/components/AddTransaction.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddTransaction = () => {
+
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  console.log(text, amount);
+
   return (
     <div>
       <h3>Add New Transaction</h3>
       <form>
         <div className="form-control">
           <label htmlFor='text'>Text</label>
-          <input type="text" id="text" placeholder='Enter Description Here...' />
+          <input type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder='Enter Description Here...' />
         </div>
         <div className="form-control">
           <label htmlFor='amount'>Amount <br />
             (Negative for Expense, Positive for Income)
           </label>
-          <input type='number' id='amount' placeholder='Enter Amount Here...'></input>
+          <input type='number'
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder='Enter Amount Here...'></input>
         </div>
         <button className="btn">Add Transaction</button>
       </form>
