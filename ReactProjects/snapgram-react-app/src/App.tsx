0ee0a3533431bee78a3a9a172e92d2248aef9bf7
Jonny Soth <janysoth@gmail.com>
2024-05-14 01:38:09 -0500

    Install react-router.

--- ReactProjects/snapgram-react-app/src/App.tsx
+++ ReactProjects/snapgram-react-app/src/App.tsx
@@ -1,10 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
 import './globals.css';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className='flex h-screen'>
+      <Routes>
+
+      </Routes>
+    </main>
   )
 }
 
