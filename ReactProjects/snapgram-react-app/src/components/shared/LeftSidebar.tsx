e74b37740155a34cc11c0d1064b8e3d9a3025e84
Jonny Soth <janysoth@gmail.com>
2024-05-24 01:06:19 -0500

    Add className for the p tag in the button.

--- ReactProjects/snapgram-react-app/src/components/shared/LeftSidebar.tsx
+++ ReactProjects/snapgram-react-app/src/components/shared/LeftSidebar.tsx
@@ -72,9 +72,15 @@ const LeftSidebar = () => {
         </ul>
       </div>
 
-      <Button variant="ghost" className='shad-button_ghost' onClick={() => signOut}>
+      <Button
+        variant="ghost"
+        className='shad-button_ghost'
+        onClick={() => signOut}
+      >
         <img src="/assets/icons/logout.svg" alt="logout" />
-        Logout
+        <p className="small-medium lg:base-medium">
+          Logout
+        </p>
       </Button>
     </nav>
   )
