66f5e2d29f1b04840e32f93c2146615d6ea92991
Jonny Soth <janysoth@gmail.com>
2024-06-06 05:42:43 -0500

    Fix the signOut function in Topbar.tsx

--- ReactProjects/snapgram-react-app/src/components/shared/Topbar.tsx
+++ ReactProjects/snapgram-react-app/src/components/shared/Topbar.tsx
@@ -5,9 +5,9 @@ import { useEffect } from 'react';
 import { useUserContext } from '@/context/AuthContext';
 
 const Topbar = () => {
-  const { mutate: signOut, isSuccess } = useSignOutAccount();
   const navigate = useNavigate();
   const { user } = useUserContext();
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
 
   useEffect(() => {
     if (isSuccess) navigate(0);
@@ -16,7 +16,7 @@ const Topbar = () => {
   return (
     <section className="topbar">
       <div className="flex-between py-4 px-5">
-        <Link to="/" className='flex gap-3 items-center'>
+        <Link to="/" className="flex gap-3 items-center">
           <img
             src="/assets/images/logo.svg"
             alt="logo"
@@ -25,23 +25,24 @@ const Topbar = () => {
           />
         </Link>
 
-        <div className="flex gap-3">
-          <Button variant="ghost" className='shad-button_ghost' onClick={() => signOut}>
+        <div className="flex gap-4">
+          <Button
+            variant="ghost"
+            className="shad-button_ghost"
+            onClick={() => signOut()}>
             <img src="/assets/icons/logout.svg" alt="logout" />
           </Button>
-
-          {/* Link to navigate to the profile */}
-          <Link to={`/profile/${user.id}`} className='flex-center gap-3'>
+          <Link to={`/profile/${user.id}`} className="flex-center gap-3">
             <img
-              src={user.imageUrl || '/assets/images/profile-placeholder.svg'}
-              alt='profile'
-              className='h-8 w-8 rounded-full'
+              src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+              alt="profile"
+              className="h-8 w-8 rounded-full"
             />
           </Link>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Topbar
+export default Topbar;
