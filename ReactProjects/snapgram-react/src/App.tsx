76adbe0bbd33c7b2bfa6056ff7b5d16f2384b10a
Jonny Soth <janysoth@gmail.com>
2024-06-12 05:46:57 -0500

    Create all of the pages and export them into App.tsx

--- ReactProjects/snapgram-react/src/App.tsx
+++ ReactProjects/snapgram-react/src/App.tsx
@@ -4,7 +4,17 @@ import "./globals.css";
 import { Toaster } from "./components/ui/toaster";
 import SignupForm from "@/_auth/forms/SignupForm";
 import SigninForm from "@/_auth/forms/SigninForm";
-import { Home } from "./_root/pages";
+import {
+  Home,
+  Explore,
+  Saved,
+  CreatePost,
+  Profile,
+  EditPost,
+  PostDetails,
+  UpdateProfile,
+  AllUsers,
+} from "@/_root/pages";
 import AuthLayout from "./_auth/forms/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 
@@ -21,6 +31,14 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
 
