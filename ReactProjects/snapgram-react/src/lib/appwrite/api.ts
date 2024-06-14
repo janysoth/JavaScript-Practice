ef12e1884f60d2b5d34010c8b9a1a802671e9592
Jonny Soth <janysoth@gmail.com>
2024-06-14 05:44:22 -0500

    Create functions to like, save, and delete posts.

--- ReactProjects/snapgram-react/src/lib/appwrite/api.ts
+++ ReactProjects/snapgram-react/src/lib/appwrite/api.ts
@@ -199,5 +199,69 @@ export async function getRecentPosts() {
 
   return posts;
 }
+// ============================== LIKE POSTS
+export async function likePost(postId: string, likesArray: string[]) {
+  try {
+    const updatedPost = await databases.updateDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.postCollectionId,
+      postId,
+      {
+        likes: likesArray
+      }
+    );
 
+    if (!updatedPost) {
+      console.log("Like Post did not get updated")
+      throw Error
+    }
 
+    return updatedPost;
+  } catch (error) {
+    console.log(error, "likePost failed.")
+  }
+}
+
+// ============================== SAVE POSTS
+export async function savePost(postId: string, userId: string) {
+  try {
+    const updatedPost = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.savesCollectionId,
+      ID.unique(),
+      {
+        user: userId,
+        post: postId,
+      }
+    );
+
+    if (!updatedPost) {
+      console.log("Save Post did not get updated")
+      throw Error
+    }
+
+    return updatedPost;
+  } catch (error) {
+    console.log(error, "savePost failed.")
+  }
+}
+
+// ============================== DELETE POSTS
+export async function deleteSavedPost(savedRecordId: string) {
+  try {
+    const statusCode = await databases.deleteDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.savesCollectionId,
+      savedRecordId,
+    );
+
+    if (!statusCode) {
+      console.log("There's no statusCode.")
+      throw Error
+    }
+
+    return { status: "ok" };
+  } catch (error) {
+    console.log(error, "deletePost failed.")
+  }
+}
