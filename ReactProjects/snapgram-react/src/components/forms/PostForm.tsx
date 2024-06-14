fb75c3630fc91195cca640fa389e5f5498ea5788
Jonny Soth <janysoth@gmail.com>
2024-06-14 01:43:32 -0500

    Create createPost function to use in PostForm component.

--- ReactProjects/snapgram-react/src/components/forms/PostForm.tsx
+++ ReactProjects/snapgram-react/src/components/forms/PostForm.tsx
@@ -1,41 +1,59 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+import { Models } from "appwrite"
 
 import { Button } from "@/components/ui/button"
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+  Form, FormControl, FormField, FormItem, FormLabel, FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "../ui/textarea"
 import FileUploader from "../shared/FileUploader"
+import { PostValidation } from "@/lib/validation"
+import { useCreatePost } from "@/lib/react-query/queriesAndMutations"
+import { useUserContext } from "@/context/AuthContext"
+import { useNavigate } from "react-router-dom"
+import { useToast } from "../ui/use-toast"
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+type PostFormProps = {
+  post?: Models.Document;
+  action: "Create" | "Update";
+};
 
-const PostForm = ({ post }) => {
+const PostForm = ({ post }: PostFormProps) => {
+  const { user } = useUserContext();
+  const navigate = useNavigate();
+  const { toast } = useToast();
+
+  const { mutateAsync: createPost, isPending: isLoadingCreate } = useCreatePost();
+
+
 
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof PostValidation>>({
+    resolver: zodResolver(PostValidation),
     defaultValues: {
-      username: "",
+      caption: post ? post?.caption : "",
+      file: [],
+      location: post ? post.location : "",
+      tags: post ? post.tags.join(",") : "",
     },
   })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof PostValidation>) {
+    const newPost = await createPost({
+      ...values,
+      userId: user.id,
+    })
+
+    if (!newPost) {
+      toast({
+        title: `Please try again.`,
+      });
+    }
+    navigate("/");
   }
 
   return (
@@ -87,6 +105,7 @@ const PostForm = ({ post }) => {
                 <Input
                   type="text"
                   className="shad-input"
+                  {...field}
                 />
               </FormControl>
               <FormMessage className="shad-form_message" />
@@ -106,6 +125,7 @@ const PostForm = ({ post }) => {
                   type="text"
                   className="shad-input"
                   placeholder="Art, Expression, Learn"
+                  {...field}
                 />
               </FormControl>
               <FormMessage className="shad-form_message" />
--- ReactProjects/snapgram-react/src/lib/react-query/queriesAndMutations.ts
+++ ReactProjects/snapgram-react/src/lib/react-query/queriesAndMutations.ts
@@ -4,8 +4,9 @@ import {
   useQueryClient,
   useInfiniteQuery,
 } from "@tanstack/react-query";
-import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
-import { INewUser } from "@/types";
+import { createPost, createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
+import { INewPost, INewUser } from "@/types";
+import { QUERY_KEYS } from "@/lib/react-query/queryKeys";
 
 export const useCreateUserAccount = () => {
   return useMutation({
@@ -23,6 +24,18 @@ export const useSignInAccount = () => {
 export const useSignOutAccount = () => {
   return useMutation({
     mutationFn: signOutAccount,
+  });
+};
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+    },
   });
 };
 
