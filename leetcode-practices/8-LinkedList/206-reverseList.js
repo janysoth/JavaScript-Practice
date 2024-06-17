f31062823fd99cc0658e24fe2652f19b349333fb
janysoth <janysoth@gmail.com>
2024-06-17 02:19:34 -0500

    Refactor the codes.

--- leetcode-practices/8-LinkedList/206-reverseList.js
+++ leetcode-practices/8-LinkedList/206-reverseList.js
@@ -2,30 +2,25 @@
  * Given the head of a singly linked list, reverse the list, and return the reversed list.
  */
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
+class ListNode {
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
 const reverseList = function (head) {
-
-  if (!head || !head.next) return head;
-
-  let prev = null;
-  let current = head;
-
-  while (current !== null) {
-    let next = current.next;
-    current.next = prev;
-    prev = current;
-    current = next;
+  // Edge case: empty list or single node
+  if (!head || !head.next) {
+    return head;
   }
 
-  return prev; // prev will be the new head of the reversed list
+  // Recursive call to reverse the rest of the list
+  const reversedHead = reverseList(head.next);
+
+  // Adjust pointers to reverse the current node
+  head.next.next = head;  // reverse the next node's pointer
+  head.next = null;       // set current node's next pointer to null
+
+  return reversedHead;    // return the new head of the reversed list
 };
