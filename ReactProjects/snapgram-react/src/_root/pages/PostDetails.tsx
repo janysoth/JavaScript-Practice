21311ca0cd2be36a0b21cb4e8fee3e92fa4d1c2e
Jonny Soth <janysoth@gmail.com>
2024-06-20 05:06:37 -0500

    Add the PostStats component in the PostDetails.

--- ReactProjects/snapgram-react/src/_root/pages/PostDetails.tsx
+++ ReactProjects/snapgram-react/src/_root/pages/PostDetails.tsx
@@ -1,4 +1,5 @@
 import Loader from "@/components/shared/Loader";
+import PostStats from "@/components/shared/PostStats";
 import { Button } from "@/components/ui/button";
 import { useUserContext } from "@/context/AuthContext";
 import { useGetPostById } from "@/lib/react-query/queriesAndMutations"
@@ -87,7 +88,7 @@ const PostDetails = () => {
 
             <hr className="border w-full border-dark-4/80" />
 
-            <div className="small-medium lg:base-medium py-5">
+            <div className="flex flex-col flex-1 w-full small-medium lg:base-regular">
               <p>{post?.caption}</p>
               <ul className="flex gap-1 mt-2">
                 {post?.tags.map((tag: string) => (
@@ -96,6 +97,10 @@ const PostDetails = () => {
                   </li>
                 ))}
               </ul>
+            </div>
+
+            <div className="w-full">
+              <PostStats post={post} userId={user.id} />
             </div>
           </div>
         </div>
