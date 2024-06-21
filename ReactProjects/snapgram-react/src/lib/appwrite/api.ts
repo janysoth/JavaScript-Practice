6142b53cb724988547a11b9f93846bfeef7ffb7a
Jonny Soth <janysoth@gmail.com>
2024-06-21 01:24:19 -0500

    Create getInfinitePosts and searchPosts function in api.ts file.

--- ReactProjects/snapgram-react/src/lib/appwrite/api.ts
+++ ReactProjects/snapgram-react/src/lib/appwrite/api.ts
@@ -354,5 +354,48 @@ export async function deletePost(postId: string, imageId: string) {
 
 }
 
+// ============================== INFINITE POST
+export async function getInfinitePosts({ pageParam }: { pageParam: number }) {
+
+  const queries: any[] = [Query.orderDesc('$updatedAt'), Query.limit(10)];
+
+  if (pageParam) {
+    queries.push(Query.cursorAfter(pageParam.toString()));
+  }
+
+  try {
+    const posts = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.postCollectionId,
+      queries
+    )
+
+    if (!posts) throw Error;
+
+    return posts;
+  } catch (error) {
+    console.log(error, "getInfinitePosts failed.")
+  }
+}
+
+// ============================== SEARCH POST
+export async function searchPosts(searchTerm: string) {
+
+  try {
+    const posts = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.postCollectionId,
+      [Query.search('caption', searchTerm)]
+    )
+
+    if (!posts) throw Error;
+
+    return posts;
+  } catch (error) {
+    console.log(error, "getInfinitePosts failed.")
+  }
+}
+
+
 
 
