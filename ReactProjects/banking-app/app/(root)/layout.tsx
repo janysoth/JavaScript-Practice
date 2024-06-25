d6c0c4c8e1bea5c5a4344abf598564f7dc5e5e3d
Jonny Soth <janysoth@gmail.com>
2024-06-25 05:33:46 -0500

    create a markup for Sidebar component.

--- ReactProjects/banking-app/app/(root)/layout.tsx
+++ ReactProjects/banking-app/app/(root)/layout.tsx
@@ -1,12 +1,15 @@
+import Sidebar from "@/components/Sidebar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firtsName: 'Jonny Vorn', lastName: 'Soth' };
+
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
       {children}
     </main>
   );
