a4bcf8ad1b1261a337d63a2db0abc1c33aa1f1f4
Jonny Soth <janysoth@gmail.com>
2024-06-26 02:44:57 -0500

    finish styling the sidebar component using cn function created in utils.ts file.

--- ReactProjects/banking-app/components/Sidebar.tsx
+++ ReactProjects/banking-app/components/Sidebar.tsx
@@ -1,14 +1,21 @@
+// Turn this into a Client side component
+'use client'
+
 import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const Sidebar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname();
+
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
         <Link
           href="/"
-          className='mb-12 cursor-pointer items-center gap-2'
+          className='flex mb-12 cursor-pointer items-center gap-2'
         >
           <Image
             src="/icons/logo.svg"
@@ -24,14 +31,37 @@ const Sidebar = ({ user }: SiderbarProps) => {
         </Link>
 
         {sidebarLinks.map((item) => {
+          const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
+
           return (
-            <Link href={item.route} key={item.label}>
-              {item.label}
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn('sidebar-link', {
+                'bg-bank-gradient': isActive
+              })}
+            >
+              <div className="relative size-6">
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({
+                    'brightness-[3] invert-0': isActive
+                  })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
             </Link>
           )
         })}
+
+        USER
       </nav>
 
+      FOOTER
     </section>
   )
 }
