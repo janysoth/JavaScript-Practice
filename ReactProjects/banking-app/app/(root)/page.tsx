669a8eb49593aadb892529b7f432d171ebd33e36
Jonny Soth <janysoth@gmail.com>
2024-07-09 02:48:14 -0500

    Updated Home component to fetch logged in user data asynchronously and updated RightSidebar to use 'name' instead of 'firstName'.

--- ReactProjects/banking-app/app/(root)/page.tsx
+++ ReactProjects/banking-app/app/(root)/page.tsx
@@ -1,14 +1,11 @@
 import HeaderBox from "@/components/HeaderBox"
 import RightSideBar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-const Home = () => {
+const Home = async () => {
 
-  const loggedIn = {
-    firstName: 'Jonny',
-    lastName: 'Soth',
-    email: 'janysoth@gmail.com'
-  };
+  const loggedIn = await getLoggedInUser();
 
   return (
     <section className="home">
@@ -17,7 +14,7 @@ const Home = () => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || 'Guest'}
+            user={loggedIn?.name || 'Guest'}
             subtext="Access and manage your account and transactions efficiently"
           />
 
