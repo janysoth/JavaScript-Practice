a301062a4353f6fb8adf7b9712a5e8c619cd6dee
Jonny Soth <janysoth@gmail.com>
2024-07-18 03:06:11 -0500

    Create markup for TransactionsTable component. Add shadcn-ui table.

---
+++ ReactProjects/banking-app/components/TransactionsTable.tsx
@@ -0,0 +1,45 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { formatAmount, getTransactionStatus } from "@/lib/utils"
+
+const TransactionsTable = ({ transactions }: TransactionTableProps) => {
+  return (
+    <Table>
+      <TableHeader className="bg-[#f9fafb]">
+        <TableRow>
+          <TableHead className="px-2">Transaction</TableHead>
+          <TableHead className="px-2">Amount</TableHead>
+          <TableHead className="px-2">Status</TableHead>
+          <TableHead className="px-2">Date</TableHead>
+          <TableHead className="px-2 max-md:hidden">Channel</TableHead>
+          <TableHead className="px-2 max-md:hidden">Category</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {transactions.map((t: Transaction) => {
+          const status = getTransactionStatus(new Date(t.date));
+          const amount = formatAmount(t.amount);
+
+          const isDebit = t.type === 'debit';
+          const isCredit = t.type === 'credit';
+
+          return (
+            <TableRow>
+
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
+
+  )
+}
+
+export default TransactionsTable
