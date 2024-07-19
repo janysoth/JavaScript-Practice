eb73dbb5891782187ca28b488688bc3035a633a2
Jonny Soth <janysoth@gmail.com>
2024-07-19 01:44:05 -0500

    Refactor TransactionsTable component: add CategoryBadge component, update table cell styles and formatting, and modified transaction status and category displays.

--- ReactProjects/banking-app/components/TransactionsTable.tsx
+++ ReactProjects/banking-app/components/TransactionsTable.tsx
@@ -7,7 +7,25 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import { formatAmount, formatDateTime, getTransactionStatus, removeSpecialCharacters } from "@/lib/utils"
+import { transactionCategoryStyles } from "@/constants"
+import { cn, formatAmount, formatDateTime, getTransactionStatus, removeSpecialCharacters } from "@/lib/utils"
+
+// Component inside of component without import
+const CategoryBadge = ({ category }: CategoryBadgeProps) => {
+  const {
+    borderColor,
+    backgroundColor,
+    textColor,
+    chipBackgroundColor,
+  } = transactionCategoryStyles[category as keyof typeof transactionCategoryStyles] || transactionCategoryStyles.default
+
+  return (
+    <div className={cn('category-badge', borderColor, chipBackgroundColor)}>
+      <div className={cn('size-2 rounded-full', backgroundColor)} />
+      <p className={cn('text-[12px] font-medium', textColor)}>{category}</p>
+    </div>
+  )
+}
 
 const TransactionsTable = ({ transactions }: TransactionTableProps) => {
   return (
@@ -31,39 +49,47 @@ const TransactionsTable = ({ transactions }: TransactionTableProps) => {
           const isCredit = t.type === 'credit';
 
           return (
-            <TableRow key={t.id}>
+            <TableRow
+              key={t.id}
+              className={`${isDebit || amount[0] === '-' ? 'bg-[#FFFBFA]' : 'bg-[#F6FEF9]'} !over:bg-none !border-b-DEFAULT`}
+            >
               {/* Transaction Table Cell */}
-              <TableCell>
-                <div>
-                  <h1>
+              <TableCell className="max-w-[250px] pl-2 pr-10">
+                <div className="flex items-center gap-3">
+                  <h1 className="text-14 truncate font-semibold text-[#344054]">
                     {removeSpecialCharacters(t.name)}
                   </h1>
                 </div>
               </TableCell>
 
               {/* Amount Table Cell */}
-              <TableCell>
+              <TableCell
+                className={`pl-2 pr-10 font-semibold ${isDebit || amount[0] === '-' ?
+                  'text-[#f04438]'
+                  : 'text-[#039855]'
+                  }`}
+              >
                 {isDebit ? `-${amount}` : isCredit ? amount : amount}
               </TableCell>
 
               {/* Status Table Cell */}
-              <TableCell>
-                {status}
+              <TableCell className="pl-2 pr-10">
+                <CategoryBadge category={status} />
               </TableCell>
 
               {/* Date Table Cell */}
-              <TableCell>
+              <TableCell className="min-w-32 pl-2 pr-10">
                 {formatDateTime(new Date(t.date)).dateTime}
               </TableCell>
 
               {/* Payment Channel Table Cell */}
-              <TableCell>
+              <TableCell className="pl-2 pr-10 capitalize min-w-24 max-md:hidden">
                 {t.paymentChannel}
               </TableCell>
 
               {/* Category Table Cell */}
-              <TableCell>
-                {t.category}
+              <TableCell className="pl-2 pr-10 max-md:hidden">
+                <CategoryBadge category={t.category} />
               </TableCell>
 
             </TableRow>
