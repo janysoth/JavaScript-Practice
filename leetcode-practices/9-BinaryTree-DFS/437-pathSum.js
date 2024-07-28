3204e9ea6fdb1f2cfa02a2180693263c472e1f29
Jonny Vorn Soth <janysoth@gmail.com>
2024-07-27 22:53:20 -0500

    Create question 437 of LeetCode to solve.

---
+++ leetcode-practices/9-BinaryTree-DFS/437-pathSum.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ * 
+ * Given the root of a binary tree and an integer targetSum, return the number of paths where the sum of the values along the path equals targetSum.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (i.e., traveling only from parent nodes to child nodes).
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number}
+ */
+var pathSum = function (root, targetSum) {
+
+};
