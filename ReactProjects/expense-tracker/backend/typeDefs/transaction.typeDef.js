4d8b39c276628e115eab6935bf59b7f77af9be73
Jonny Vorn Soth <janysoth@gmail.com>
2024-07-31 02:03:35 -0500

    Fix some typos in transaction.typeDef.js

--- ReactProjects/expense-tracker/backend/typeDefs/transaction.typeDef.js
+++ ReactProjects/expense-tracker/backend/typeDefs/transaction.typeDef.js
@@ -7,19 +7,26 @@ const transactionTypeDef = `#graphql
     paymentType: String!
     category: String!
     amount: Float!
-    location: string
+    location: String
     date: String!
+    user: User!
   }
 
   type Query {
-    transaction: [Transaction!]
+    transactions: [Transaction!]
     transaction(transactionId:ID!): Transaction
+    categoryStatistics: [categoryStatistics!]
   }
 
   type Mutation {
     createTransaction(input: CreateTransactionInput!): Transaction!
-    updateTransaction(input: updateTransaction!): Transaction!
+    updateTransaction(input: UpdateTransactionInput!): Transaction!
     deleteTransaction(transactionId:ID!): Transaction!
+  }
+  
+  type categoryStatistics {
+    category: String!
+    totalAmount: Float!
   }
 
   input CreateTransactionInput {
@@ -28,7 +35,7 @@ const transactionTypeDef = `#graphql
     category: String!
     amount: Float!
     date: String!
-    location: String!
+    location: String
   }
 
   input UpdateTransactionInput {
