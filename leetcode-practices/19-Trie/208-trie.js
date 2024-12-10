1a0e391892f31e1881ed0d6e3fff6ce1515c5c38
Jonny Vorn Soth <janysoth@gmail.com>
2024-12-09 23:56:37 -0600

    Refactor Trie implementation to use Sets for storing full words and prefixes.

--- leetcode-practices/19-Trie/208-trie.js
+++ leetcode-practices/19-Trie/208-trie.js
@@ -1,65 +1,53 @@
-/**
- * A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
-
-Implement the Trie class:
-
-Trie() Initializes the trie object.
-void insert(String word) Inserts the string word into the trie.
-boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
-boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
- */
-
-class TrieNode {
+class Trie {
   constructor() {
     // A set to store full words inserted into the Trie
     this.fullWords = new Set();
     // A set to store all prefixes of the inserted words
     this.prefixes = new Set();
   }
-}
 
-const Trie = function () {
-  this.root = {}; // Root of the Trie
-};
+  /**
+   * Inserts a word into the Trie.
+   * @param {string} word
+   */
+  insert(word) {
+    if (!this.fullWords) throw new Error("fullWords not initialized properly");
+    // Add the complete word to the set of full words
+    this.fullWords.add(word);
 
-/** 
-* @param {string} word
-* @return {void}
-*/
-Trie.prototype.insert = function (word) {
-  // Add the complete word to the set of full words
-  this.fullWords.add(word);
+    // Add all prefixes of the word to the set of prefixes
+    let currentPrefix = word;
+    for (let i = word.length - 1; i >= 0; i--) {
+      // If the prefix already exists, stop processing further
+      if (this.prefixes.has(currentPrefix)) {
+        break;
+      }
 
-  // Add all prefixes of the word to the set of prefixes
-  let currentPrefix = word;
-
-  for (let i = word.length - 1; i >= 0; i++) {
-    // If the prefix already exists, stop processing further
-    if (this.prefixes.has(currentPrefix)) break;
-
-    // Add the prefix to the set 
-    this.prefixes.add(currentPrefix);
-
-    // Shorten the prefix by removing the last character
-    currentPrefix = currentPrefix.slice(0, i);
+      // Add the prefix to the set
+      this.prefixes.add(currentPrefix);
+      // Shorten the prefix by removing the last character
+      currentPrefix = currentPrefix.slice(0, i);
+    }
   }
-};
 
-/** 
-* @param {string} word
-* @return {boolean}
-*/
-Trie.prototype.search = function (word) {
-  return this.fullWords.has(word);
-};
+  /**
+   * Checks if the word exists in the Trie.
+   * @param {string} word
+   * @return {boolean}
+   */
+  search(word) {
+    return this.fullWords.has(word);
+  }
 
-/** 
-* @param {string} prefix
-* @return {boolean}
-*/
-Trie.prototype.startsWith = function (prefix) {
-  return this.prefixes.has(prefix);
-};
+  /**
+   * Checks if any word in the Trie starts with the given prefix.
+   * @param {string} prefix
+   * @return {boolean}
+   */
+  startsWith(prefix) {
+    return this.prefixes.has(prefix);
+  }
+}
 
 // Example usage:
 const trie = new Trie();
