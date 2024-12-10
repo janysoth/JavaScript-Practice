13c7385d28c6c833d5e58801dd92d890c37b0878
Jonny Vorn Soth <janysoth@gmail.com>
2024-12-10 02:57:52 -0600

    Solve question 1268 of LeetCode.

---
+++ leetcode-practices/19-Trie/1268-suggestedProducts.js
@@ -0,0 +1,43 @@
+/**
+ * You are given an array of strings products and a string searchWord.
+
+Design a system that suggests at most three product names from products after each character of searchWord is typed. Suggested products should have common prefix with searchWord. If there are more than three products with a common prefix return the three lexicographically minimums products.
+
+Return a list of lists of the suggested products after each character of searchWord is typed.
+ */
+
+/**
+ * @param {string[]} products
+ * @param {string} searchWord
+ * @return {string[][]}
+ */
+const suggestedProducts = function (products, searchWord) {
+  products.sort();
+
+  let result = [];
+  let prefix = "";
+
+  for (let char of searchWord) {
+    // Update the prefix with the current character
+    prefix += char;
+
+    let suggestions = [];
+
+    for (let product of products) {
+      if (product.startsWith(prefix))
+        suggestions.push(product);
+
+      // Limit to 3 suggestions
+      if (suggestions.length === 3) break;
+    }
+
+    result.push(suggestions);
+  }
+
+  return result;
+};
+
+// Example Usage
+let products = ["mobile", "mouse", "moneypot", "monitor", "mousepad"];
+let searchWord = "mouse";
+console.log(suggestedProducts(products, searchWord));
